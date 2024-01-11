ac96e0a5bef36ed6ead51b30403009d336b46f8e
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-01-11 14:00:17 +0800

    feat(offchain): enhance `Arena` module

--- offchain/core/src/strategy/mod.rs
+++ offchain/core/src/strategy/mod.rs
@@ -1,2 +1,5 @@
+//! This module defines the struct [Player] that is responsible for reacting to the states
+//! of tournaments; and the struct [GarbageCollector] that is responsible for collecting finished matches
+
 pub mod gc;
 pub mod player;
