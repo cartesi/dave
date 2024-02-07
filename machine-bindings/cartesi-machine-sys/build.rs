fa01162b2ea5f224756792b44248e2ea7f43bc75
Sofia Rodrigues <algebraic.sofia+github@gmail.com>
2024-02-07 18:43:28 -0300

    refactor: small changes and add headers

--- machine-bindings/cartesi-machine-sys/build.rs
+++ machine-bindings/cartesi-machine-sys/build.rs
@@ -1,3 +1,15 @@
+use std::{env, path::PathBuf};
+
 fn main() {
     println!("cargo:rustc-link-lib=cartesi");
+
+    let bindings = bindgen::Builder::default()
+        .header("src/headers/machine-c-api.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- machine-bindings/cartesi-machine-sys/src/lib.rs
+++ machine-bindings/cartesi-machine-sys/src/lib.rs
@@ -1,4 +1,7 @@
 #![doc = include_str!("../README.md")]
 
-pub mod bindings;
-pub use bindings::*;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
