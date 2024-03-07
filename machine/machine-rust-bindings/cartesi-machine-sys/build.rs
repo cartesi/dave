8281cba838f90ec4e44176b61b2170b9de23a881
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-03-07 14:50:46 -0700

    feat: embed `libcartesi.a`

--- machine/machine-rust-bindings/cartesi-machine-sys/build.rs
+++ machine/machine-rust-bindings/cartesi-machine-sys/build.rs
@@ -1,13 +1,57 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, process::Command};
 
 fn main() {
-    println!("cargo:rustc-link-lib=cartesi");
+    println!("cargo:rerun-if-changed=build.rs");
 
+    // Directory where `libcartesi.a` is located after it's built.
+    let machine_dir_path = PathBuf::from("../../machine-emulator")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
+    let libdir_path = machine_dir_path.join("src");
+    println!("cargo:rerun-if-changed={}", libdir_path.to_str().unwrap());
+
+    // Build `libcartesi.a`
+    Command::new("make")
+        .current_dir(&machine_dir_path)
+        .args(&["submodules", "downloads", "dep"])
+        .output()
+        .expect("Failed to run setup `make submodules downloads dep`");
+    Command::new("make")
+        .current_dir(&machine_dir_path)
+        .args(&["bundle-boost"])
+        .output()
+        .expect("Failed to run `make bundle-boost`");
+    Command::new("make")
+        .current_dir(&machine_dir_path)
+        .args(&[
+            "-C",
+            "src",
+            "release=yes",
+            "libcartesi.a",
+            "libcartesi_jsonrpc.a",
+        ])
+        .output()
+        .expect("Failed to build `libcartesi.a`");
+
+    // Tell cargo where to look for libraries, and which libraries to link
+    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=cartesi");
+
+    println!(
+        "cargo:rerun-if-changed={}",
+        libdir_path.join("machine-c-api.h").to_str().unwrap()
+    );
+
+    // Generate bindings
     let bindings = bindgen::Builder::default()
-        .header("src/headers/machine-c-api.h")
+        .header(libdir_path.join("machine-c-api.h").to_str().unwrap())
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
--- machine/machine-rust-bindings/cartesi-machine-sys/src/lib.rs
+++ machine/machine-rust-bindings/cartesi-machine-sys/src/lib.rs
@@ -1,7 +1,8 @@
 #![doc = include_str!("../README.md")]
-
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+extern crate link_cplusplus;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
