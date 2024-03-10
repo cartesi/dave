259271cc5d7a2e4abc8fb615a553d1c32c450f0b
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-03-10 17:21:48 -0600

    feat: rename `rollups` to `cmio`

--- machine/machine-rust-bindings/cartesi-machine/src/configuration.rs
+++ machine/machine-rust-bindings/cartesi-machine/src/configuration.rs
@@ -65,10 +65,235 @@ impl From<cartesi_machine_sys::cm_htif_config> for HtifConfig {
     }
 }
 
+/// CmIo configuration
+#[derive(Debug, Clone)]
+pub struct CmIoConfig {
+    /// Represents whether the rest of the struct have been filled
+    pub has_value: bool,
+    /// RX buffer memory range
+    pub rx_buffer: MemoryRangeConfig,
+    /// TX buffer memory range
+    pub tx_buffer: MemoryRangeConfig,
+}
+
+impl From<CmIoConfig> for cartesi_machine_sys::cm_cmio_config {
+    fn from(config: CmIoConfig) -> Self {
+        Self {
+            has_value: config.has_value,
+            rx_buffer: config.rx_buffer.into(),
+            tx_buffer: config.tx_buffer.into(),
+        }
+    }
+}
+
+impl From<cartesi_machine_sys::cm_cmio_config> for CmIoConfig {
+    fn from(config: cartesi_machine_sys::cm_cmio_config) -> Self {
+        Self {
+            has_value: config.has_value,
+            rx_buffer: config.rx_buffer.into(),
+            tx_buffer: config.tx_buffer.into(),
+        }
+    }
+}
+
+/// Uarch RAM configuration
+#[derive(Debug, Clone)]
+pub struct UarchRamConfig {
+    /// RAM image file name
+    pub image_filename: Option<String>,
+}
+
+impl From<UarchRamConfig> for cartesi_machine_sys::cm_uarch_ram_config {
+    fn from(config: UarchRamConfig) -> Self {
+        Self {
+            image_filename: to_cstr(config.image_filename),
+        }
+    }
+}
+
+impl From<cartesi_machine_sys::cm_uarch_ram_config> for UarchRamConfig {
+    fn from(config: cartesi_machine_sys::cm_uarch_ram_config) -> Self {
+        Self {
+            image_filename: from_cstr(config.image_filename),
+        }
+    }
+}
+
+/// Uarch Processor configuration
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct UarchProcessorConfig {
+    /// General purpose registers
+    pub x: [u64; 32usize],
+    /// Program counter
+    pub pc: u64,
+    /// Machine cycle
+    pub cycle: u64,
+    /// Halt flag
+    pub halt_flag: bool,
+}
+
+impl From<UarchProcessorConfig> for cartesi_machine_sys::cm_uarch_processor_config {
+    fn from(config: UarchProcessorConfig) -> Self {
+        unsafe { std::mem::transmute(config) }
+    }
+}
+
+impl From<cartesi_machine_sys::cm_uarch_processor_config> for UarchProcessorConfig {
+    fn from(config: cartesi_machine_sys::cm_uarch_processor_config) -> Self {
+        unsafe { std::mem::transmute(config) }
+    }
+}
+
+/// Uarch configuration
+#[derive(Debug, Clone)]
+pub struct UarchConfig {
+    /// Processor configuration
+    pub processor: UarchProcessorConfig,
+    /// RAM configuration
+    pub ram: UarchRamConfig,
+}
+
+impl From<UarchConfig> for cartesi_machine_sys::cm_uarch_config {
+    fn from(config: UarchConfig) -> Self {
+        Self {
+            processor: config.processor.into(),
+            ram: config.ram.into(),
+        }
+    }
+}
+
+impl From<cartesi_machine_sys::cm_uarch_config> for UarchConfig {
+    fn from(config: cartesi_machine_sys::cm_uarch_config) -> Self {
+        Self {
+            processor: config.processor.into(),
+            ram: config.ram.into(),
+        }
+    }
+}
+
+/// Machine configuration
+#[derive(Debug, Clone)]
+pub struct MachineConfig {
+    /// Processor configuration
+    pub processor: ProcessorConfig,
+    /// RAM configuration
+    pub ram: RamConfig,
+    /// DTB configuration
+    pub dtb: DtbConfig,
+    /// Flash drive configuration
+    pub flash_drive: Vec<MemoryRangeConfig>,
+    /// TLB configuration
+    pub tlb: TlbConfig,
+    /// CLint configuration
+    pub clint: ClintConfig,
+    /// Htif configuration
+    pub htif: HtifConfig,
+    /// CmIo configuration
+    pub cmio: CmIoConfig,
+    /// Uarch configuration
+    pub uarch: UarchConfig,
+}
+
+impl From<MachineConfig> for OwnedMachineConfig {
+    fn from(config: MachineConfig) -> Self {
+        let count = config.flash_drive.len();
+        let leaked_entry = Box::leak(
+            config
+                .flash_drive
+                .into_iter()
+                .map(|x| x.into())
+                .collect::<Vec<_>>()
+                .into_boxed_slice(),
+        );
+        let entry: *mut cartesi_machine_sys::cm_memory_range_config = leaked_entry.as_mut_ptr();
+        let flash_drive = cartesi_machine_sys::cm_memory_range_config_array { entry, count };
+
+        let config = cartesi_machine_sys::cm_machine_config {
+            processor: config.processor.into(),
+            ram: config.ram.into(),
+            dtb: config.dtb.into(),
+            flash_drive,
+            tlb: config.tlb.into(),
+            clint: config.clint.into(),
+            htif: config.htif.into(),
+            cmio: config.cmio.into(),
+            uarch: config.uarch.into(),
+        };
+
+        Self(config)
+    }
+}
+
+impl From<cartesi_machine_sys::cm_machine_config> for MachineConfig {
+    fn from(value: cartesi_machine_sys::cm_machine_config) -> Self {
+        Self {
+            processor: value.processor.into(),
+            ram: value.ram.into(),
+            dtb: value.dtb.into(),
+            flash_drive: unsafe {
+                std::slice::from_raw_parts(
+                    value.flash_drive.entry,
+                    value.flash_drive.count as usize,
+                )
+                .into_iter()
+                .map(|x| x.clone().into())
+                .collect()
+            },
+            tlb: value.tlb.into(),
+            clint: value.clint.into(),
+            htif: value.htif.into(),
+            cmio: value.cmio.into(),
+            uarch: value.uarch.into(),
+        }
+    }
+}
+
+impl Default for MachineConfig {
+    fn default() -> Self {
+        unsafe {
+            let raw_config = cartesi_machine_sys::cm_new_default_machine_config();
+            let config = MachineConfig::from(*raw_config);
+            cartesi_machine_sys::cm_delete_machine_config(raw_config);
+            config
+        }
+    }
+}
+
+/// A machine configuration that is owned by Rust and should be dropped in another way.
+pub struct OwnedMachineConfig(cartesi_machine_sys::cm_machine_config);
+
+impl AsRef<cartesi_machine_sys::cm_machine_config> for OwnedMachineConfig {
+    fn as_ref(&self) -> &cartesi_machine_sys::cm_machine_config {
+        &self.0
+    }
+}
+
+impl Drop for OwnedMachineConfig {
+    fn drop(&mut self) {
+        free_cstr(self.0.ram.image_filename);
+        free_cstr(self.0.dtb.bootargs);
+        free_cstr(self.0.dtb.init);
+        free_cstr(self.0.dtb.entrypoint);
+        free_cstr(self.0.dtb.image_filename);
+        free_cstr(self.0.tlb.image_filename);
+        free_cstr(self.0.cmio.rx_buffer.image_filename);
+        free_cstr(self.0.cmio.tx_buffer.image_filename);
+        free_cstr(self.0.uarch.ram.image_filename);
+
+        unsafe {
+            drop(Box::from_raw(std::slice::from_raw_parts_mut(
+                self.0.flash_drive.entry,
+                self.0.flash_drive.count as usize,
+            )))
+        }
+    }
+}
+
 #[derive(Debug, Default, Clone)]
 #[repr(C)]
 pub struct ConcurrencyRuntimeConfig {
-    update_merkle_tree: u64,
+    pub update_merkle_tree: u64,
 }
 
 impl From<ConcurrencyRuntimeConfig> for cartesi_machine_sys::cm_concurrency_runtime_config {
@@ -86,7 +311,7 @@ impl From<cartesi_machine_sys::cm_concurrency_runtime_config> for ConcurrencyRun
 #[derive(Debug, Default, Clone)]
 #[repr(C)]
 pub struct HtifRuntimeConfig {
-    no_console_putchar: bool,
+    pub no_console_putchar: bool,
 }
 
 impl From<HtifRuntimeConfig> for cartesi_machine_sys::cm_htif_runtime_config {
