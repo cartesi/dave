00f29b1e66826771d474125b9d0b5873d4de17cf
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-03-10 21:27:11 -0600

    fix: fix include of generated binding

--- machine/machine-rust-bindings/cartesi-machine-sys/src/lib.rs
+++ machine/machine-rust-bindings/cartesi-machine-sys/src/lib.rs
@@ -5,4 +5,4 @@
 
 extern crate link_cplusplus;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-include!(concat!(env!("OUT_DIR"), "/htif_defines.rs"));
+include!(concat!(env!("OUT_DIR"), "/htif.rs"));
