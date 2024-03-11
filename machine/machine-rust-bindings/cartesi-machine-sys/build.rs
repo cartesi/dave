de5d4764f590756f5edce3c564474fb6f8dfbbed
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-03-10 20:49:36 -0600

    feat: add htif constants
    Fix machine break reason.
    Refactor code to more idiomatic Rust.

--- machine/machine-rust-bindings/cartesi-machine-sys/build.rs
+++ machine/machine-rust-bindings/cartesi-machine-sys/build.rs
@@ -39,21 +39,23 @@ fn main() {
     println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=cartesi");
 
-    println!(
-        "cargo:rerun-if-changed={}",
-        libdir_path.join("machine-c-api.h").to_str().unwrap()
-    );
-
     // Generate bindings
-    let bindings = bindgen::Builder::default()
+    let machine_bindings = bindgen::Builder::default()
         .header(libdir_path.join("machine-c-api.h").to_str().unwrap())
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+    let htif = bindgen::Builder::default()
+        .header(libdir_path.join("htif-defines.h").to_str().unwrap())
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
+    machine_bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Couldn't write machine bindings!");
+    htif.write_to_file(out_path.join("htif.rs"))
+        .expect("Couldn't write htif defines!");
 }
