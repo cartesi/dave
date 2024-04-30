1abe8070197696d75361e0139b0d91f3b6fc49c1
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-05-01 07:41:15 +0800

    chore: improve code quality

--- permissionless-arbitration/lua_node/utils/helper.lua
+++ permissionless-arbitration/lua_node/utils/helper.lua
@@ -3,15 +3,16 @@ local color = require "utils.color"
 local names = {'green', 'yellow', 'blue', 'pink', 'cyan', 'white'}
 local idle_template = [[ls player%d_idle 2>/dev/null | grep player%d_idle | wc -l]]
 local ps_template = [[ps %s | grep defunct | wc -l]]
+local helper = {}
 
-local function log(player_index, msg)
+function helper.log(player_index, msg)
     local color_index = (player_index - 1) % #names + 1
     local timestamp = os.date("%m/%d/%Y %X")
     print(color.reset .. color.fg[names[color_index]] ..
         string.format("[#%d][%s] %s", player_index, timestamp, msg) .. color.reset)
 end
 
-local function log_to_ts(reader, last_ts)
+function helper.log_to_ts(reader, last_ts)
     -- print everything hold in the buffer which has smaller timestamp
     -- this is to synchronise when there're gaps in between the logs
     local msg_output = 0
@@ -36,14 +37,14 @@ local function log_to_ts(reader, last_ts)
     return last_ts, msg_output
 end
 
-local function is_zombie(pid)
+function helper.is_zombie(pid)
     local reader = io.popen(string.format(ps_template, pid))
     local ret = reader:read()
     reader:close()
     return tonumber(ret) == 1
 end
 
-local function stop_players(pid_reader)
+function helper.stop_players(pid_reader)
     for pid, reader in pairs(pid_reader) do
         print(string.format("Stopping player with pid %s...", pid))
         os.execute(string.format("kill -15 %s", pid))
@@ -52,52 +53,42 @@ local function stop_players(pid_reader)
     end
 end
 
-local function str_to_bool(str)
+function helper.str_to_bool(str)
     if str == nil then
         return false
     end
     return string.lower(str) == 'true'
 end
 
-local function touch_player_idle(player_index)
+function helper.touch_player_idle(player_index)
     os.execute(string.format("touch player%d_idle", player_index))
 end
 
-local function is_player_idle(player_index)
+function helper.is_player_idle(player_index)
     local reader = io.popen(string.format(idle_template, player_index, player_index))
     local ret = reader:read()
     reader:close()
     return tonumber(ret) == 1
 end
 
-local function rm_player_idle(player_index)
+function helper.rm_player_idle(player_index)
     os.execute(string.format("rm player%d_idle 2>/dev/null", player_index))
 end
 
-local function all_players_idle(pid_player)
+function helper.all_players_idle(pid_player)
     for _, player in pairs(pid_player) do
-        if not is_player_idle(player) then
+        if not helper.is_player_idle(player) then
             return false
         end
     end
     return true
 end
 
-local function rm_all_players_idle(pid_player)
+function helper.rm_all_players_idle(pid_player)
     for _, player in pairs(pid_player) do
-        rm_player_idle(player)
+        helper.rm_player_idle(player)
     end
     return true
 end
 
-return {
-    log = log,
-    log_to_ts = log_to_ts,
-    is_zombie = is_zombie,
-    stop_players = stop_players,
-    touch_player_idle = touch_player_idle,
-    str_to_bool = str_to_bool,
-    all_players_idle = all_players_idle,
-    rm_all_players_idle = rm_all_players_idle,
-    rm_player_idle = rm_player_idle
-}
+return helper
