7334156be4ffd5224375cf4321e18d521afaf787
Guilherme Dantas <guidanoli@proton.me>
2024-04-30 16:16:53 -0300

    refactor: deploy contracts with `forge script`

--- permissionless-arbitration/contracts/deploy_anvil.sh
+++ permissionless-arbitration/contracts/deploy_anvil.sh
@@ -1,29 +1,15 @@
-#!/bin/bash
-RPC_URL="http://127.0.0.1:8545"
-PK="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-CONCRETE_FACTORIES=("SingleLevelTournamentFactory" \
-                    "TopTournamentFactory" \
-                    "MiddleTournamentFactory" \
-                    "BottomTournamentFactory")
+#!/usr/bin/env bash
 
-deployed_factories=()
-for factory in ${CONCRETE_FACTORIES[@]}; do
-    deployed_at=`forge create $factory --rpc-url=$RPC_URL --private-key=$PK | \
-                grep "Deployed to: " | \
-                tr -d '\n' | \
-                tail -c 42`
-    deployed_factories+=($deployed_at)
-done
+set -euo pipefail
 
-deployed_at=`forge create TournamentFactory --rpc-url=$RPC_URL --private-key=$PK --constructor-args \
-    ${deployed_factories[0]} \
-    ${deployed_factories[1]} \
-    ${deployed_factories[2]} \
-    ${deployed_factories[3]} | \
-    grep "Deployed to: " | \
-    tr -d '\n' | \
-    tail -c 42`
+INITIAL_HASH=`xxd -p -c32 "${MACHINE_PATH}/hash"`
 
-initial_hash=`xxd -p -c32 $MACHINE_PATH/hash`
+export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-cast send --private-key=$PK --rpc-url=$RPC_URL $deployed_at "instantiateTop(bytes32)" 0x$initial_hash
+forge script \
+    script/TopTournament.s.sol \
+    --fork-url "http://127.0.0.1:8545" \
+    --broadcast \
+    --sig "run(bytes32)" \
+    "${INITIAL_HASH}" \
+    -vvvv
