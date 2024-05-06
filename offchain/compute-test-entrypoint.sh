a5f0b6c6ffee42c4f99510c238029d8235de5001
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-05-06 20:54:05 +0800

    chore: polish scripts

--- offchain/compute-test-entrypoint.sh
+++ offchain/compute-test-entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 # integration test with lua node
 cd /root/permissionless-arbitration && ./lua_node/attached_entrypoint.lua &
--- permissionless-arbitration/lua_node/doom_showcase/view.sh
+++ permissionless-arbitration/lua_node/doom_showcase/view.sh
@@ -1,22 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
 cd snapshots
 snapshot_count=0
 while true
 do
-    snapshot_files=($( ls -dtr -1 * ))
+    snapshot_files=($( ls -dtr -1 * || echo "" ))
     new_snapshot_count=${#snapshot_files[@]}
-    # echo $new_snapshot_count
-    if [ "$snapshot_count" -ne "$new_snapshot_count" ]; then
+    if [ "$snapshot_count" -ne "$new_snapshot_count" ] &&
+    [ -d "${snapshot_files[snapshot_count]}/pixels" ]; then
         cd ${snapshot_files[snapshot_count]}/pixels
         while true
         do
-            if [ ! -f "done" ]; then
-                mplayer -mf fps=30 -x 640 -y 400 -geometry 50%:50% 'mf://*.png'
-            else
+            if [ -f "done" ]; then
                 mplayer -mf fps=30 -x 640 -y 400 -geometry 50%:50% 'mf://*.png'
                 break
+            else
+                mplayer -mf fps=30 -x 640 -y 400 -geometry 50%:50% 'mf://*.png'
             fi
         done
+        log2_stride=($( cat ../log2_stride ))
+        if [ "$log2_stride" == "0" ];then
+            # dave process reaches its leaf tournament when log2_stride is 0
+            break
+        fi
         snapshot_count=$((snapshot_count+1))
         cd -
     fi
