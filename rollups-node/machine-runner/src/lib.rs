b0b7570d819d926be1283ff33cf4e3d67465c5d7
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-05-23 17:40:38 +0800

    feat: implement `start` of `machine-runner`

--- rollups-node/machine-runner/src/lib.rs
+++ rollups-node/machine-runner/src/lib.rs
@@ -1,11 +1,7 @@
-use std::{
-    path::Path,
-    sync::{Arc, Mutex},
-};
+use anyhow::Result;
+use std::{path::Path, sync::Arc, time::SystemTime};
 
-use cartesi_machine::{
-    configuration::RuntimeConfig, errors::MachineError, BreakReason, HtifYieldReason,
-};
+use cartesi_machine::{configuration::RuntimeConfig, BreakReason, HtifYieldReason};
 use rollups_state_manager::StateManager;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -19,18 +15,18 @@ pub struct Machine {
 }
 
 impl Machine {
-    pub fn load(path: &Path, runtime: RuntimeConfig) -> Result<Machine, MachineError> {
+    pub fn load(path: &Path, runtime: RuntimeConfig) -> Result<Machine> {
         let machine = cartesi_machine::Machine::load(path, runtime)?;
         Ok(Self { machine })
     }
 
-    fn feed(&mut self, input: &[u8]) -> Result<(), MachineError> {
+    fn feed(&mut self, input: &[u8]) -> Result<()> {
         self.machine
             .send_cmio_response(HtifYieldReason::HtifYieldReasonAdvanceState, input)?;
         Ok(())
     }
 
-    pub fn advance(&mut self, data: &[u8]) -> Result<Halted, MachineError> {
+    pub fn advance(&mut self, data: &[u8]) -> Result<Halted> {
         loop {
             self.machine.reset_iflags_y()?;
             self.feed(data)?;
@@ -45,32 +41,44 @@ impl Machine {
         }
     }
 
-    pub async fn start(&mut self, _s: Arc<StateManager>) -> Result<(), MachineError> {
-        //load the latest cached machine snapshot, sort the machine snapshots cached by key to get the latest one
-        // ```
+    pub async fn start(
+        s: Arc<StateManager>,
+        initial_machine: &str,
+        snapshot_frequency: u64,
+    ) -> Result<()> {
+        let (snapshot, mut epoch_number, mut next_input_index) = match s.latest_snapshot()? {
+            Some(r) => (r.0, r.1, r.2 + 1),
+            None => (initial_machine.to_string(), 0, 0),
+        };
+        let mut machine = Machine::load(&Path::new(&snapshot), RuntimeConfig::default())?;
+        let mut now = SystemTime::now();
 
-        // tick
-        // ```
-        // loop {
+        loop {
+            match s.input(epoch_number, next_input_index) {
+                Ok(input) => {
+                    machine.advance(&input)?;
+                    let machine_state = machine.machine.get_root_hash()?;
+                    s.add_state(machine_state.as_bytes(), epoch_number, next_input_index)?;
 
-        // calculate next-input from meta-counter
-        // loop {
-        //  if not next-input then break;
-
-        //  add next-input to machine and run until yield
-        //  increment state meta-counter
-        //  increment var next-input
-        // }
-
-        // if self.epoch == db.curr_epoch, break
-        // elseif self.epoch < curr_epoch:
-        //  - calculate computation-hash (db read)
-        //  - write computation-hash to db
-        //  - reset self.meta-counter, increment self.epoch
-        //  - continue
-
-        // } // end outer loop
-
-        Ok(())
+                    if now.elapsed()?.as_secs() > (snapshot_frequency * 60) {
+                        // take snapshot every 20 minutes
+                        let path = machine_state.to_string();
+                        machine.machine.store(&Path::new(&path))?;
+                        s.add_snapshot(&path, epoch_number, next_input_index)?;
+                        now = SystemTime::now();
+                    }
+                    next_input_index += 1;
+                }
+                Err(_) => {
+                    // fail to get next input, try get input 0 from next epoch
+                    if s.input(epoch_number + 1, 0).is_ok() {
+                        // new epoch starts and current epoch closes
+                        // TODO: calculate computation-hash
+                        epoch_number += 1;
+                        next_input_index = 0;
+                    }
+                }
+            }
+        }
     }
 }
