94ba6c59e8babcdf2b565adc2dba46b045e22f21
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-06-02 17:40:31 +0800

    feat: refactor `blockchain-reader` - remove `concurrency` and `semaphore` - update `start` method - simplify struct fields - fix inclusiveness of `get_events` - add `AddressBook` type - add proper `error` type - use `tokio::time::sleep`

---
+++ rollups-node/blockchain-reader/src/error.rs
@@ -0,0 +1,36 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+use rollups_state_manager::StateManager;
+
+use ethers::abi::Error as AbiError;
+use ethers::providers::ProviderError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub struct ProviderErrors(pub Vec<ProviderError>);
+
+impl std::fmt::Display for ProviderErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Provider error: {:?}", self.0)
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum BlockchainReaderError<SM: StateManager> {
+    #[error(transparent)]
+    Providers {
+        #[from]
+        source: ProviderErrors,
+    },
+
+    #[error(transparent)]
+    Abi {
+        #[from]
+        source: AbiError,
+    },
+
+    StateManagerError(<SM as StateManager>::Error),
+}
+
+pub type Result<T, SM> = std::result::Result<T, BlockchainReaderError<SM>>;
