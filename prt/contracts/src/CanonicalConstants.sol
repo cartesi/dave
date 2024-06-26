22684b6c826778ab21f2cd371c7b572305abe601
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-06-26 19:59:59 +0800

    feat: use proper emulator/tournament constants

--- prt/contracts/src/CanonicalConstants.sol
+++ prt/contracts/src/CanonicalConstants.sol
@@ -6,42 +6,46 @@ pragma solidity ^0.8.17;
 import "./Time.sol";
 
 library ArbitrationConstants {
-    // maximum tolerance time for participant being censored
-    // Time.Duration constant CENSORSHIP_TOLERANCE =
-    //     Time.Duration.wrap(60 * 60 * 24 * 7);
-
     // maximum time for computing the commitments offchain
+    // should be root_tournament_slowdown * (direct final state execution time of the application)
     // Time.Duration constant COMMITMENT_EFFORT =
     //     Time.Duration.wrap(60 * 60 * 4); // TODO
 
-    // maximum time for interacting with a divergence search (match)
+    // maximum tolerance time for participant being censored, relating to the blockchain loading
+    // should be constant
+    // Time.Duration constant CENSORSHIP_TOLERANCE =
+    //     Time.Duration.wrap(60 * 60 * 24 * 7);
+
+    // maximum time for interacting with a divergence search (match), relating to the height of the match
+    // should be constant
     // Time.Duration constant MATCH_EFFORT =
     //     Time.Duration.wrap(60 * 60); // TODO
+
+    Time.Duration constant COMMITMENT_EFFORT = Time.Duration.wrap(60 * 50);
+    Time.Duration constant CENSORSHIP_TOLERANCE = Time.Duration.wrap(60 * 5);
+    Time.Duration constant MATCH_EFFORT = Time.Duration.wrap(60 * 2);
 
     Time.Duration constant MAX_ALLOWANCE = Time.Duration.wrap(
         Time.Duration.unwrap(CENSORSHIP_TOLERANCE)
             + Time.Duration.unwrap(COMMITMENT_EFFORT)
     );
 
-    Time.Duration constant COMMITMENT_EFFORT = Time.Duration.wrap(60 * 50);
-    Time.Duration constant CENSORSHIP_TOLERANCE = Time.Duration.wrap(60 * 5);
-    Time.Duration constant MATCH_EFFORT = Time.Duration.wrap(60 * 2);
+    uint64 constant LOG2_UARCH_SPAN = 20;
+    uint64 constant LOG2_EMULATOR_SPAN = 48;
+    uint64 constant LOG2_INPUT_SPAN = LOG2_UARCH_SPAN + LOG2_EMULATOR_SPAN;
 
     // 3-level tournament
-    uint64 constant LEVELS = 4;
-    uint64 constant LOG2_MAX_MCYCLE = 16;
+    uint64 constant LEVELS = 3;
 
     /// @return log2step gap of each leaf in the tournament[level]
     function log2step(uint64 level) internal pure returns (uint64) {
-        uint64[LEVELS] memory arr =
-            [uint64(49), uint64(35), uint64(19), uint64(0)];
+        uint64[LEVELS] memory arr = [uint64(49), uint64(35), uint64(0)];
         return arr[level];
     }
 
     /// @return height of the tournament[level] tree which is calculated by subtracting the log2step[level] from the log2step[level - 1]
     function height(uint64 level) internal pure returns (uint64) {
-        uint64[LEVELS] memory arr =
-            [uint64(14), uint64(14), uint64(16), uint64(19)];
+        uint64[LEVELS] memory arr = [uint64(19), uint64(14), uint64(35)];
         return arr[level];
     }
 }
