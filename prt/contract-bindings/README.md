849e4de6df140a5587a5038b905bf4a0c4e3f208
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-08-13 10:15:37 +0800

    feat: simplify `contract-bindings`

--- prt/contract-bindings/README.md
+++ prt/contract-bindings/README.md
@@ -1,1 +1,8 @@
 # PRT Contract bindings for Rust
+
+## Generate Rust bindings
+
+Run make command from [parent directory](../)
+```
+make bind
+```
--- prt/contract-bindings/src/lib.rs
+++ prt/contract-bindings/src/lib.rs
@@ -1,8 +1,2 @@
-#![allow(clippy::all)]
-
-pub mod leaf_tournament;
-pub mod non_leaf_tournament;
-pub mod non_root_tournament;
-pub mod root_tournament;
-pub mod shared_types;
-pub mod tournament;
+pub mod contract;
+pub use contract::*;
