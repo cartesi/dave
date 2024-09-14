604b68406c30cc8690aa92de68c0d76977d9ec89
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2024-09-14 07:00:35 -0300

    chore(prt-test): improve scoped require example

--- prt/client-lua/player/player.lua
+++ prt/client-lua/player/player.lua
@@ -1,7 +1,15 @@
 local StateFetcher = require "player.state"
+local HonestStrategy = require "player.strategy"
+local CommitmentBuilder = require "computation.commitment"
+local Sender = require "player.sender"
 
-local function new(tournament_address, strategy, blokchain_endpoint, hook)
+local function new(tournament_address, wallet, machine_path, blokchain_endpoint, hook)
     local state_fetcher = StateFetcher:new(tournament_address, blokchain_endpoint)
+    local strategy = HonestStrategy:new(
+        CommitmentBuilder:new(machine_path),
+        machine_path,
+        Sender:new(wallet.pk, wallet.player_id, blokchain_endpoint)
+    )
 
     local function react()
         local state = state_fetcher:fetch()
@@ -15,10 +23,11 @@ local function new(tournament_address, strategy, blokchain_endpoint, hook)
     end
 
     return coroutine.create(function()
-        while true do
-            local log = react()
+        local log
+        repeat
+            log = react()
             coroutine.yield(log)
-        end
+        until log.finished
     end)
 end
 
