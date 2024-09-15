db65a030e9510a25b8a5db01e4e6f2ccc0894450
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2024-09-15 17:15:21 +0800

    feat: support clib in `scoped_require`

--- prt/tests/compute/utils/scoped_require.lua
+++ prt/tests/compute/utils/scoped_require.lua
@@ -1,11 +1,34 @@
-local function find_chunk(name)
-    local x, err = package.searchpath(name, package.path)
+local function find_path(name)
+    local x, err1 = package.searchpath(name, package.path)
 
-    if err then
-        error(string.format("module '%s' not found\n", name))
+    if not err1 then
+        return "lua", x
     end
 
-    return x
+    local y, err2 = package.searchpath(name, package.cpath)
+
+    if not err2 then
+        return "clib", y
+    end
+
+    error(string.format("module '%s' not found\n", name))
+end
+
+local function custom_require_c(library_path, library_name)
+    -- Load the library
+    local mylib = package.loadlib(library_path, "luaopen_" .. library_name)
+
+    local module, err
+    if not mylib then
+        module = nil
+        err = "Failed to load library: " .. library_path
+    else
+        -- Call the library's initialization function
+        module = mylib()
+        err = false
+    end
+
+    return module, err
 end
 
 local function new_scoped_require(env)
@@ -15,12 +38,23 @@ local function new_scoped_require(env)
 
     local function scoped_require(name)
         if loaded[name] == nil then
-            local path = find_chunk(name)
+            local module_type, path = find_path(name)
 
-            local chunk, err = loadfile(path, "t", new_env)
+            local chunk, err, result
+            if module_type == "lua" then
+                chunk, err = loadfile(path, "t", new_env)
+            elseif module_type == "clib" then
+                chunk, err = custom_require_c(path, name)
+            end
+
             if not chunk then error(err) end
 
-            local result = { chunk(name) }
+            if module_type == "lua" then
+                result = { chunk(name) }
+            elseif module_type == "clib" then
+                result = { chunk }
+            end
+
             loaded[name] = result
         end
 
