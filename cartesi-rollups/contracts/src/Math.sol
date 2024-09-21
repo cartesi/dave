8bdc7f8a5fad99d5e90b73b30cd9903a2b2e1eb7
Guilherme Dantas <guidanoli@proton.me>
2024-09-21 01:30:17 -0300

    fix: apply @GCdePaula's suggestions
    - Create deeper stack for Merkle root calculation
    - Rename `parent` as `join` in `Merkle` library
    - Remove unused functions from `Math` library

--- cartesi-rollups/contracts/src/Math.sol
+++ cartesi-rollups/contracts/src/Math.sol
@@ -5,22 +5,6 @@ pragma solidity ^0.8.0;
 
 /// @author Felipe Argento
 library Math {
-    /// @param a the dividend
-    /// @param b the log2 of the divisor
-    /// @return ceil(a/2^b)
-    function ceilDivExp2(uint256 a, uint256 b) internal pure returns (uint256) {
-        return (a + (1 << b) - 1) >> b;
-    }
-
-    /// @notice get floor of log2 of number
-    /// @param x number to take floor(log2) of
-    /// @return floor(log2) of x
-    function getLog2Floor(uint256 x) internal pure returns (uint8) {
-        require(x != 0, "log of zero is undefined");
-
-        return uint8(255 - clz(x));
-    }
-
     /// @notice count trailing zeros
     /// @param x number you want the ctz of
     /// @dev this a binary search implementation
@@ -57,48 +41,6 @@ library Math {
             x = x >> 2;
         }
         if (x & 0x0000000000000000000000000000000000000000000000000000000000000001 == 0) {
-            n = n + 1;
-        }
-
-        return n;
-    }
-
-    /// @notice count leading zeros
-    /// @param x number you want the clz of
-    /// @dev this a binary search implementation
-    function clz(uint256 x) internal pure returns (uint256) {
-        if (x == 0) return 256;
-
-        uint256 n = 0;
-        if (x & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000 == 0) {
-            n = n + 128;
-            x = x << 128;
-        }
-        if (x & 0xFFFFFFFFFFFFFFFF000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 64;
-            x = x << 64;
-        }
-        if (x & 0xFFFFFFFF00000000000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 32;
-            x = x << 32;
-        }
-        if (x & 0xFFFF000000000000000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 16;
-            x = x << 16;
-        }
-        if (x & 0xFF00000000000000000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 8;
-            x = x << 8;
-        }
-        if (x & 0xF000000000000000000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 4;
-            x = x << 4;
-        }
-        if (x & 0xC000000000000000000000000000000000000000000000000000000000000000 == 0) {
-            n = n + 2;
-            x = x << 2;
-        }
-        if (x & 0x8000000000000000000000000000000000000000000000000000000000000000 == 0) {
             n = n + 1;
         }
 
