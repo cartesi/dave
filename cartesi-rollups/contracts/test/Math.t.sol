84fd797a68fe78ae604413b8514063db0d3a02b3
Guilherme Dantas <guidanoli@proton.me>
2024-10-23 14:33:28 -0300

    test: add more test cases

--- cartesi-rollups/contracts/test/Math.t.sol
+++ cartesi-rollups/contracts/test/Math.t.sol
@@ -39,8 +39,12 @@ library NaiveMath {
 contract MathTest is Test {
     function testCtz() external pure {
         assertEq(Math.ctz(0), 256);
+        assertEq(Math.ctz(type(uint256).max), 0);
         for (uint256 i; i < 256; ++i) {
             assertEq(Math.ctz(1 << i), i);
+            for (uint256 j = i + 1; j < 256; ++j) {
+                assertEq(Math.ctz((1 << i) | (1 << j)), i);
+            }
         }
     }
 
@@ -50,8 +54,12 @@ contract MathTest is Test {
 
     function testClz() external pure {
         assertEq(Math.clz(0), 256);
+        assertEq(Math.clz(type(uint256).max), 0);
         for (uint256 i; i < 256; ++i) {
             assertEq(Math.clz(1 << i), 255 - i);
+            for (uint256 j; j < i; ++j) {
+                assertEq(Math.clz((1 << i) | (1 << j)), 255 - i);
+            }
         }
     }
 
@@ -61,8 +69,12 @@ contract MathTest is Test {
 
     function testLog2Clp() external pure {
         assertEq(Math.log2clp(0), 0);
+        assertEq(Math.log2clp(type(uint256).max), 256);
         for (uint256 i; i < 256; ++i) {
             assertEq(Math.log2clp(1 << i), i);
+            for (uint256 j; j < i; ++j) {
+                assertEq(Math.log2clp((1 << i) | (1 << j)), i + 1);
+            }
         }
     }
 
