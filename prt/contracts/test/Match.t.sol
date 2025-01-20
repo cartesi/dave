8261355d1e250659d979b2398f73b6a021d6501a
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2025-01-20 15:47:16 +0800

    test(prt-contracts): enable `Match` unit tests

--- prt/contracts/test/Match.t.sol
+++ prt/contracts/test/Match.t.sol
@@ -18,19 +18,14 @@ import "src/CanonicalConstants.sol";
 
 pragma solidity ^0.8.0;
 
-// TODO: we cannot set the height of a match anymore
-// To properly test, we'll need to swap the implementation of
-// ArbitrationConstants.
-// Or wait until `mockCall` works on internal calls `https://github.com/foundry-rs/foundry/issues/432`
-
-/*
 contract MatchTest is Test {
     using Tree for Tree.Node;
     using Machine for Machine.Hash;
     using Match for Match.Id;
+    using Match for Match.IdHash;
     using Match for Match.State;
 
-    uint256 MAX_LOG2_SIZE = ArbitrationConstants.height(0);
+    Tree.Node constant ONE_NODE = Tree.Node.wrap(bytes32(uint256(1)));
 
     Match.State leftDivergenceMatch;
     Match.State rightDivergenceMatch;
@@ -38,26 +33,20 @@ contract MatchTest is Test {
     Match.IdHash rightDivergenceMatchId;
 
     function setUp() public {
-        Tree.Node leftDivergenceCommitment1 = Tree.ZERO_NODE.join(
-            Tree.ZERO_NODE
-        );
-        Tree.Node rightDivergenceCommitment1 = Tree.ZERO_NODE.join(
-            Tree.ZERO_NODE
-        );
+        Tree.Node leftDivergenceCommitment1 =
+            Tree.ZERO_NODE.join(Tree.ZERO_NODE);
+        Tree.Node rightDivergenceCommitment1 =
+            Tree.ZERO_NODE.join(Tree.ZERO_NODE);
 
-        Tree.Node leftDivergenceCommitment2 = Tree
-            .Node
-            .wrap(bytes32(uint256(1)))
-            .join(Tree.ZERO_NODE);
-        Tree.Node rightDivergenceCommitment2 = Tree.ZERO_NODE.join(
-            Tree.Node.wrap(bytes32(uint256(1)))
-        );
+        Tree.Node leftDivergenceCommitment2 = ONE_NODE.join(Tree.ZERO_NODE);
+        Tree.Node rightDivergenceCommitment2 = Tree.ZERO_NODE.join(ONE_NODE);
 
         (leftDivergenceMatchId, leftDivergenceMatch) = Match.createMatch(
             leftDivergenceCommitment1,
             leftDivergenceCommitment2,
-            Tree.Node.wrap(bytes32(uint256(1))),
+            ONE_NODE,
             Tree.ZERO_NODE,
+            0,
             1
         );
 
@@ -65,7 +54,8 @@ contract MatchTest is Test {
             rightDivergenceCommitment1,
             rightDivergenceCommitment2,
             Tree.ZERO_NODE,
-            Tree.Node.wrap(bytes32(uint256(1))),
+            ONE_NODE,
+            0,
             1
         );
     }
@@ -75,22 +65,17 @@ contract MatchTest is Test {
             !leftDivergenceMatch.agreesOnLeftNode(Tree.ZERO_NODE),
             "left node should diverge"
         );
-        (
-            Machine.Hash _finalHashOne,
-            Machine.Hash _finalHashTwo
-        ) = leftDivergenceMatch.setDivergenceOnLeftLeaf(Tree.ZERO_NODE);
 
         leftDivergenceMatch.height = 2;
+        (Machine.Hash _finalHashOne, Machine.Hash _finalHashTwo) =
+            leftDivergenceMatch._setDivergenceOnLeftLeaf(Tree.ZERO_NODE);
 
         assertTrue(
-            _finalHashOne.eq(Tree.ZERO_NODE.toMachineHash()),
-            "hash one should be zero"
+            _finalHashOne.eq(ONE_NODE.toMachineHash()), "hash one should be 1"
         );
         assertTrue(
-            _finalHashTwo.eq(
-                Tree.Node.wrap(bytes32(uint256(1))).toMachineHash()
-            ),
-            "hash two should be 1"
+            _finalHashTwo.eq(Tree.ZERO_NODE.toMachineHash()),
+            "hash two should be zero"
         );
     }
 
@@ -99,22 +84,17 @@ contract MatchTest is Test {
             rightDivergenceMatch.agreesOnLeftNode(Tree.ZERO_NODE),
             "left node should match"
         );
-        (
-            Machine.Hash _finalHashOne,
-            Machine.Hash _finalHashTwo
-        ) = rightDivergenceMatch.setDivergenceOnRightLeaf(Tree.ZERO_NODE);
 
         rightDivergenceMatch.height = 2;
+        (Machine.Hash _finalHashOne, Machine.Hash _finalHashTwo) =
+            rightDivergenceMatch._setDivergenceOnRightLeaf(Tree.ZERO_NODE);
 
         assertTrue(
-            _finalHashOne.eq(Tree.ZERO_NODE.toMachineHash()),
-            "hash one should be zero"
+            _finalHashOne.eq(ONE_NODE.toMachineHash()), "hash one should be 1"
         );
         assertTrue(
-            _finalHashTwo.eq(
-                Tree.Node.wrap(bytes32(uint256(1))).toMachineHash()
-            ),
-            "hash two should be 1"
+            _finalHashTwo.eq(Tree.ZERO_NODE.toMachineHash()),
+            "hash two should be zero"
         );
     }
 
@@ -123,22 +103,17 @@ contract MatchTest is Test {
             !leftDivergenceMatch.agreesOnLeftNode(Tree.ZERO_NODE),
             "left node should diverge"
         );
-        (
-            Machine.Hash _finalHashOne,
-            Machine.Hash _finalHashTwo
-        ) = leftDivergenceMatch.setDivergenceOnLeftLeaf(Tree.ZERO_NODE);
 
         leftDivergenceMatch.height = 3;
+        (Machine.Hash _finalHashOne, Machine.Hash _finalHashTwo) =
+            leftDivergenceMatch._setDivergenceOnLeftLeaf(Tree.ZERO_NODE);
 
         assertTrue(
             _finalHashOne.eq(Tree.ZERO_NODE.toMachineHash()),
             "hash one should be zero"
         );
         assertTrue(
-            _finalHashTwo.eq(
-                Tree.Node.wrap(bytes32(uint256(1))).toMachineHash()
-            ),
-            "hash two should be 1"
+            _finalHashTwo.eq(ONE_NODE.toMachineHash()), "hash two should be 1"
         );
     }
 
@@ -147,23 +122,27 @@ contract MatchTest is Test {
             rightDivergenceMatch.agreesOnLeftNode(Tree.ZERO_NODE),
             "left node should match"
         );
-        (
-            Machine.Hash _finalHashOne,
-            Machine.Hash _finalHashTwo
-        ) = rightDivergenceMatch.setDivergenceOnRightLeaf(Tree.ZERO_NODE);
 
         rightDivergenceMatch.height = 3;
+        (Machine.Hash _finalHashOne, Machine.Hash _finalHashTwo) =
+            rightDivergenceMatch._setDivergenceOnRightLeaf(Tree.ZERO_NODE);
 
         assertTrue(
             _finalHashOne.eq(Tree.ZERO_NODE.toMachineHash()),
             "hash one should be zero"
         );
         assertTrue(
-            _finalHashTwo.eq(
-                Tree.Node.wrap(bytes32(uint256(1))).toMachineHash()
-            ),
-            "hash two should be 1"
+            _finalHashTwo.eq(ONE_NODE.toMachineHash()), "hash two should be 1"
         );
     }
+
+    function testEqual() public {
+        assertTrue(leftDivergenceMatchId.eq(leftDivergenceMatchId));
+        assertTrue(rightDivergenceMatchId.eq(rightDivergenceMatchId));
+        assertTrue(!leftDivergenceMatchId.eq(rightDivergenceMatchId));
+        assertTrue(!rightDivergenceMatchId.eq(leftDivergenceMatchId));
+
+        vm.expectRevert("matches are not equal");
+        leftDivergenceMatchId.requireEq(rightDivergenceMatchId);
+    }
 }
-*/
