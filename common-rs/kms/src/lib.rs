7934b81d14517c03fccabf92d7ea1c18d402f8af
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-02-25 16:31:48 -0300

    feat(kms): enhance AWS KMS integration with key creation and client setup

--- common-rs/kms/src/lib.rs
+++ common-rs/kms/src/lib.rs
@@ -3,60 +3,74 @@ use alloy::{
     signers::{aws::AwsSigner, Signer},
 };
 use aws_config::BehaviorVersion;
+use aws_sdk_kms::types::{KeySpec, KeyUsageType};
 use std::error::Error;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub async fn create_key_sign_verify(
+    client: &aws_sdk_kms::Client,
+) -> Result<String, Box<dyn Error>> {
+    let result = client
+        .create_key()
+        .key_usage(KeyUsageType::SignVerify)
+        .key_spec(KeySpec::EccSecgP256K1)
+        .send()
+        .await?;
+
+    let key_id = result.key_metadata.ok_or("No key_id")?.key_id;
+    Ok(key_id)
+}
+
+pub async fn create_aws_client() -> Result<aws_sdk_kms::Client, Box<dyn Error>> {
+    let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
+    let region = config.region();
+    let url = config.endpoint_url();
+    println!("Region: {:?}", region); // us-east-1
+    println!("Endpoint URL: {:?}", url); // http://localhost:4566
+    let client = aws_sdk_kms::Client::new(&config);
+    Ok(client)
 }
 
 pub async fn process(
+    client: aws_sdk_kms::Client,
     key_id: &str,
     chain_id: Option<ChainId>,
     message: &str,
-) -> Result<PrimitiveSignature, Box<dyn Error>> {
+) -> Result<(PrimitiveSignature, AwsSigner), Box<dyn Error>> {
     println!(
         "Processing message: {}, chain_id: {}",
         message,
         chain_id.unwrap_or_default()
     );
-    let latest = BehaviorVersion::latest();
-    let config = aws_config::defaults(latest)
-        .region("us-east-1")
-        .load()
-        .await;
-    let region = config.region();
-    println!("Region: {:?}", region);
-    let client = aws_sdk_kms::Client::new(&config);
     let signer = AwsSigner::new(client, key_id.to_string(), chain_id).await?;
     let message = message.as_bytes();
     let signature = signer.sign_message(message).await?;
 
     println!("Signature: {:?}", signature);
 
-    Ok(signature)
+    Ok((signature, signer))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
+    #[ignore = "requires LocalStack"]
     #[tokio::test]
     async fn signer_works() {
         dotenvy::from_filename("aws.env").unwrap();
-
-        let key_id = "6410a7e0-aa64-4bf4-b140-b0e687fa9c21";
-        let msg = "Olá mundo!";
+        let client = create_aws_client().await.unwrap();
+        let key_id = create_key_sign_verify(&client).await.unwrap();
+        print!("Key ID: {}", key_id);
+        let message = "Hello world!";
         let chain_id = None;
 
-        let result = process(key_id, chain_id, msg).await;
-        assert!(result.is_ok(), "Error: {:?}", result.err());
+        let signature = process(client, &key_id, chain_id, message).await;
+        assert!(signature.is_ok(), "Error: {:?}", signature.err().unwrap());
 
-        // assert_eq!(signature.recover_address_from_msg(message)?, signer.address());
+        let (signature, signer) = signature.unwrap();
+        assert_eq!(
+            signature.recover_address_from_msg(message).unwrap(),
+            signer.address()
+        );
     }
 }
