37a525c125e9f01cb50b853e54a923e144cb14f9
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-02-26 13:13:03 -0300

    feat(kms): implement KmsSigner and KmsSignerBuilder for improved key management and signing

--- common-rs/kms/src/lib.rs
+++ common-rs/kms/src/lib.rs
@@ -3,42 +3,97 @@ use alloy::{
     signers::{aws::AwsSigner, Signer},
 };
 use aws_config::BehaviorVersion;
-use aws_sdk_kms::types::{KeySpec, KeyUsageType};
+use aws_sdk_kms::{
+    types::{KeySpec, KeyUsageType},
+    Client,
+};
 use std::error::Error;
 
-pub async fn create_key_sign_verify(
-    client: &aws_sdk_kms::Client,
-) -> Result<String, Box<dyn Error>> {
-    let result = client
-        .create_key()
-        .key_usage(KeyUsageType::SignVerify)
-        .key_spec(KeySpec::EccSecgP256K1)
-        .send()
-        .await?;
-
-    let key_id = result.key_metadata.ok_or("No key_id")?.key_id;
-    Ok(key_id)
-}
-
-pub async fn create_aws_client() -> Result<aws_sdk_kms::Client, Box<dyn Error>> {
-    let config = aws_config::defaults(BehaviorVersion::v2024_03_28())
-        .load()
-        .await;
-    let client = aws_sdk_kms::Client::new(&config);
-    Ok(client)
-}
-
-pub async fn process(
-    client: aws_sdk_kms::Client,
-    key_id: &str,
+pub struct KmsSignerBuilder {
+    client: Client,
+    key_id: Option<String>,
     chain_id: Option<ChainId>,
-    message: &str,
-) -> Result<(PrimitiveSignature, AwsSigner), Box<dyn Error>> {
-    let signer = AwsSigner::new(client, key_id.to_string(), chain_id).await?;
-    let message = message.as_bytes();
-    let signature = signer.sign_message(message).await?;
+}
 
-    Ok((signature, signer))
+pub struct KmsSigner {
+    client: Client,
+    key_id: String,
+    chain_id: Option<ChainId>,
+}
+
+impl KmsSignerBuilder {
+    pub async fn new() -> Self {
+        let config = aws_config::defaults(BehaviorVersion::v2024_03_28())
+            .load()
+            .await;
+        let client = Client::new(&config);
+        Self {
+            client,
+            key_id: None,
+            chain_id: None,
+        }
+    }
+
+    pub fn with_client(mut self, client: Client) -> Self {
+        self.client = client;
+        self
+    }
+
+    pub fn with_key_id(mut self, key_id: String) -> Self {
+        self.key_id = Some(key_id);
+        self
+    }
+
+    pub fn with_chain_id(mut self, chain_id: ChainId) -> Self {
+        self.chain_id = Some(chain_id);
+        self
+    }
+
+    pub async fn create_key_sign_verify(&mut self) -> Result<&str, Box<dyn Error>> {
+        let result = self
+            .client
+            .create_key()
+            .key_usage(KeyUsageType::SignVerify)
+            .key_spec(KeySpec::EccSecgP256K1)
+            .send()
+            .await?;
+
+        let metadata = result.key_metadata.ok_or("No metadata")?;
+
+        self.key_id = Some(metadata.key_id);
+
+        Ok(self.key_id.as_deref().ok_or("No key ID")?)
+    }
+
+    pub fn build(self) -> Result<KmsSigner, Box<dyn Error>> {
+        let key_id = self.key_id.ok_or("No key_id")?;
+        Ok(KmsSigner::new(self.client, key_id, self.chain_id))
+    }
+}
+
+impl KmsSigner {
+    pub fn new(client: Client, key_id: String, chain_id: Option<ChainId>) -> Self {
+        Self {
+            client,
+            key_id,
+            chain_id,
+        }
+    }
+    pub async fn sign_message(
+        &self,
+        message: &str,
+    ) -> Result<(PrimitiveSignature, AwsSigner), Box<dyn Error>> {
+        let signer =
+            AwsSigner::new(self.client.clone(), self.key_id.clone(), self.chain_id).await?;
+        let message = message.as_bytes();
+        let signature = signer.sign_message(message).await?;
+
+        Ok((signature, signer))
+    }
+
+    pub fn get_chain_id(&self) -> Option<ChainId> {
+        self.chain_id
+    }
 }
 
 #[cfg(test)]
@@ -53,7 +108,7 @@ mod kms {
 
     use super::*;
 
-    fn aws_test_credentials() {
+    fn set_aws_test_env_vars() {
         let test_credentials = Credentials::for_tests();
         set_var("AWS_ACCESS_KEY_ID", test_credentials.access_key_id());
         set_var(
@@ -79,15 +134,20 @@ mod kms {
 
         println!("Container: {:?}", container);
 
-        aws_test_credentials();
-        let client = create_aws_client().await.unwrap();
-        let key_id = create_key_sign_verify(&client).await.unwrap();
+        set_aws_test_env_vars();
+        let mut kms_signer = KmsSignerBuilder::new().await;
+
+        let key_id = kms_signer.create_key_sign_verify().await.unwrap();
         println!("Key ID: {}", key_id);
         let message = "Hello world!";
-        let chain_id = None;
 
-        println!("Processing message: {}, chain_id: {:?}", message, chain_id);
-        let signature = process(client, &key_id, chain_id, message).await;
+        let kms_signer = kms_signer.build().unwrap();
+
+        println!(
+            "Processing message: {}, chain_id: {:?}",
+            message, kms_signer.chain_id
+        );
+        let signature = kms_signer.sign_message(message).await;
         assert!(signature.is_ok(), "Error: {:?}", signature.err().unwrap());
 
         let (signature, signer) = signature.unwrap();
