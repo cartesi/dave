03df0935a867dd4645604e0a81c6c5a924904b43
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-03-14 20:26:13 -0300

    feat(kms): update AWS script to specify key usage and key spec for KMS key creation

--- common-rs/kms/aws.sh
+++ common-rs/kms/aws.sh
@@ -3,12 +3,12 @@ set -eux
 curl -X POST $AWS_ENDPOINT_URL \
     -H "Content-Type: application/x-amz-json-1.1" \
     -H "X-Amz-Target: TrentService.CreateKey" \
-    -d '{}' | jq -C
+    -d '{ "KeyUsage": "SIGN_VERIFY", "KeySpec": "ECC_SECG_P256K1" }' | jq -C
 
 KEY_ID=$(curl -X POST $AWS_ENDPOINT_URL \
     -H "Content-Type: application/x-amz-json-1.1" \
     -H "X-Amz-Target: TrentService.ListKeys" \
-    -d '{}' | jq -r ".Keys[0].KeyId // \"\"")
+    -d '{}' | jq -r ".Keys[0].KeyId")
 
 if [ -n "$KEY_ID" ]; then
     echo "export AWS_KMS_KEY_ID=\"$KEY_ID\"" | tee -a ~/.bashrc
