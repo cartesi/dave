5e2df7ddc0937af83863c2db3146c25566dcd50b
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-03-14 18:44:03 -0300

    feat(kms): update AWS script to create KMS key and store key ID in environment variable

--- common-rs/kms/aws.sh
+++ common-rs/kms/aws.sh
@@ -1,8 +1,22 @@
 set -eux
 
-curl -X POST http://localstack:4566/ \
+curl -X POST $AWS_ENDPOINT_URL \
+    -H "Content-Type: application/x-amz-json-1.1" \
+    -H "X-Amz-Target: TrentService.CreateKey" \
+    -d '{}' | jq -C
+
+KEY_ID=$(curl -X POST $AWS_ENDPOINT_URL \
     -H "Content-Type: application/x-amz-json-1.1" \
     -H "X-Amz-Target: TrentService.ListKeys" \
-    -d '{}' | jq -C
+    -d '{}' | jq -r ".Keys[0].KeyId // \"\"")
+
+if [ -n "$KEY_ID" ]; then
+    echo "export AWS_KMS_KEY_ID=\"$KEY_ID\"" | tee -a ~/.bashrc
+else
+    echo "No Key ID found."
+fi
+
+# Store AWS key ID in an environment variable for the current session
+export AWS_KMS_KEY_ID="$KEY_ID"
 
 just test-rollups-echo
