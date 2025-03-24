2dcec3651bae24ad3d05ebe4ca86defd4b6d5ef1
Pedro Argento <pedro.argento@gmail.com>
2025-03-24 15:12:30 -0300

    feature(dave-consensus) add DaveConsensus factory

---
+++ cartesi-rollups/contracts/src/DaveConsensusFactory.sol
@@ -0,0 +1,65 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.8;
+
+import {Create2} from "openzeppelin-contracts/utils/Create2.sol";
+
+import {DaveConsensus} from "./DaveConsensus.sol";
+import {IDataProvider} from "prt-contracts/IDataProvider.sol";
+import {ITournamentFactory} from "prt-contracts/ITournamentFactory.sol";
+import {IInputBox} from "rollups-contracts/inputs/IInputBox.sol";
+import {Machine} from "prt-contracts/Machine.sol";
+
+event DaveConsensusCreation(IDataProvider daveConsensus);
+
+/// @title Dave Consensus Factory
+/// @notice Allows anyone to reliably deploy a new `IDataProvider` contract.
+contract DaveConsensusFactory {
+    function newDaveConsensus(
+        IInputBox inputBox,
+        address appContract,
+        ITournamentFactory tournamentFactory,
+        Machine.Hash initialMachineStateHash
+    ) external returns (IDataProvider) {
+        IDataProvider daveConsensus =
+            new DaveConsensus(inputBox, appContract, tournamentFactory, initialMachineStateHash);
+
+        emit DaveConsensusCreation(daveConsensus);
+
+        return daveConsensus;
+    }
+
+    function newDaveConsensus(
+        IInputBox inputBox,
+        address appContract,
+        ITournamentFactory tournamentFactory,
+        Machine.Hash initialMachineStateHash,
+        bytes32 salt
+    ) external returns (IDataProvider) {
+        IDataProvider daveConsensus =
+            new DaveConsensus{salt: salt}(inputBox, appContract, tournamentFactory, initialMachineStateHash);
+
+        emit DaveConsensusCreation(daveConsensus);
+
+        return daveConsensus;
+    }
+
+    function calculateDaveConsensusAddress(
+        IInputBox inputBox,
+        address appContract,
+        ITournamentFactory tournamentFactory,
+        Machine.Hash initialMachineStateHash,
+        bytes32 salt
+    ) external view returns (address) {
+        return Create2.computeAddress(
+            salt,
+            keccak256(
+                abi.encodePacked(
+                    type(DaveConsensus).creationCode,
+                    abi.encode(inputBox, appContract, tournamentFactory, initialMachineStateHash)
+                )
+            )
+        );
+    }
+}
