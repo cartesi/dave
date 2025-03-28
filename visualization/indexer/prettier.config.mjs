f447c5b56da56548b28a4a96f6a21d13f118565f
Bruno Menezes <brunodmenezes@gmail.com>
2025-03-28 23:11:27 +1300

    feat(visualization): Indexer genesis commit, ABI generation through just + forge.

---
+++ visualization/indexer/prettier.config.mjs
@@ -0,0 +1,14 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+}
+
+export default config
