a1d993d00a993e694b30841752bf915c431bafac
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2025-04-23 16:48:50 +0800

    chore(node): merge `prt-runner` and `epoch-manager` into one module

--- cartesi-rollups/node/dave-rollups/src/main.rs
+++ cartesi-rollups/node/dave-rollups/src/main.rs
@@ -1,6 +1,6 @@
 use dave_rollups::{
-    DaveParameters, create_blockchain_reader_task, create_compute_runner_task,
-    create_epoch_manager_task, create_machine_runner_task, create_provider,
+    DaveParameters, create_blockchain_reader_task, create_epoch_manager_task,
+    create_machine_runner_task, create_provider,
 };
 use rollups_state_manager::persistent_state_access::PersistentStateAccess;
 
@@ -24,20 +24,14 @@ async fn main() -> Result<()> {
 
     let blockchain_reader_task =
         create_blockchain_reader_task(state_manager.clone(), provider.clone(), &parameters);
-
     let epoch_manager_task =
         create_epoch_manager_task(provider.clone(), state_manager.clone(), &parameters);
-
     let machine_runner_task = create_machine_runner_task(state_manager.clone(), &parameters);
-
-    let compute_runner_task =
-        create_compute_runner_task(state_manager.clone(), provider.clone(), &parameters);
 
     futures::try_join!(
         blockchain_reader_task,
         epoch_manager_task,
         machine_runner_task,
-        compute_runner_task
     )?;
 
     unreachable!("node services should run forever")
--- cartesi-rollups/node/epoch-manager/src/error.rs
+++ cartesi-rollups/node/epoch-manager/src/error.rs
@@ -1,13 +1,21 @@
 // (c) Cartesi and individual authors (see AUTHORS)
 // SPDX-License-Identifier: Apache-2.0 (see LICENSE)
 
+use alloy::contract::Error as AlloyContractError;
+
 use cartesi_prt_core::strategy::error::ReactError;
 use rollups_state_manager::StateManager;
 
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum PRTRunnerError<SM: StateManager> {
+pub enum EpochManagerError<SM: StateManager> {
+    #[error(transparent)]
+    AlloyContract {
+        #[from]
+        source: AlloyContractError,
+    },
+
     #[error(transparent)]
     React {
         #[from]
@@ -18,4 +26,4 @@ pub enum PRTRunnerError<SM: StateManager> {
     StateManagerError(<SM as StateManager>::Error),
 }
 
-pub type Result<T, SM> = std::result::Result<T, PRTRunnerError<SM>>;
+pub type Result<T, SM> = std::result::Result<T, EpochManagerError<SM>>;
