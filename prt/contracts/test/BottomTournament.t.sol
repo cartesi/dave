04b246af735bb02a03515968fb9eb29a7d987246
Guilherme Dantas <guidanoli@proton.me>
2025-04-28 14:26:24 -0300

    chore: use Soldeer for PRT contracts

--- prt/contracts/test/BottomTournament.t.sol
+++ prt/contracts/test/BottomTournament.t.sol
@@ -10,7 +10,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-import "forge-std/Test.sol";
+import "forge-std-1.9.6/src/Test.sol";
 
 import "./Util.sol";
 import "prt-contracts/tournament/factories/MultiLevelTournamentFactory.sol";
