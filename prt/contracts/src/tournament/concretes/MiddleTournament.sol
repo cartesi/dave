c175d90a30e681e27f158c17d0184134e1db2fa2
Guilherme Dantas <guidanoli@proton.me>
2025-04-30 10:04:27 -0300

    refactor!: adapt middle tournament and factory

--- prt/contracts/src/tournament/concretes/MiddleTournament.sol
+++ prt/contracts/src/tournament/concretes/MiddleTournament.sol
@@ -3,39 +3,51 @@
 
 pragma solidity ^0.8.17;
 
+import {Clones} from "@openzeppelin-contracts-5.2.0/proxy/Clones.sol";
+
 import "prt-contracts/tournament/abstracts/NonLeafTournament.sol";
 import "prt-contracts/tournament/abstracts/NonRootTournament.sol";
 
-import "prt-contracts/types/TournamentParameters.sol";
 import "prt-contracts/tournament/factories/IMultiLevelTournamentFactory.sol";
 
 /// @notice Middle tournament is non-top, non-bottom of a multi-level instance
 contract MiddleTournament is NonLeafTournament, NonRootTournament {
-    constructor(
-        Machine.Hash _initialHash,
-        Tree.Node _contestedCommitmentOne,
-        Machine.Hash _contestedFinalStateOne,
-        Tree.Node _contestedCommitmentTwo,
-        Machine.Hash _contestedFinalStateTwo,
-        Time.Duration _allowance,
-        uint256 _startCycle,
-        uint64 _level,
-        TournamentParameters memory _tournamentParameters,
-        IDataProvider _provider,
-        IMultiLevelTournamentFactory _tournamentFactory
-    )
-        NonLeafTournament(_tournamentFactory)
-        NonRootTournament(
-            _initialHash,
-            _contestedCommitmentOne,
-            _contestedFinalStateOne,
-            _contestedCommitmentTwo,
-            _contestedFinalStateTwo,
-            _allowance,
-            _startCycle,
-            _level,
-            _tournamentParameters,
-            _provider
-        )
-    {}
+    using Clones for address;
+
+    struct Args {
+        TournamentArgs tournamentArgs;
+        NonRootTournamentArgs nonRootTournamentArgs;
+        IMultiLevelTournamentFactory tournamentFactory;
+    }
+
+    function _args() internal view returns (Args memory) {
+        return abi.decode(address(this).fetchCloneArgs(), (Args));
+    }
+
+    function _tournamentArgs()
+        internal
+        view
+        override
+        returns (TournamentArgs memory)
+    {
+        return _args().tournamentArgs;
+    }
+
+    function _nonRootTournamentArgs()
+        internal
+        view
+        override
+        returns (NonRootTournamentArgs memory)
+    {
+        return _args().nonRootTournamentArgs;
+    }
+
+    function _tournamentFactory()
+        internal
+        view
+        override
+        returns (IMultiLevelTournamentFactory)
+    {
+        return _args().tournamentFactory;
+    }
 }
