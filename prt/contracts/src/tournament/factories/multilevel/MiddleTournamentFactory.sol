fec7429e72a51a3662a4051affcbb868dad1b86b
Guilherme Dantas <guidanoli@proton.me>
2025-04-30 10:04:27 -0300

    refactor!: adapt middle tournament and factory

--- prt/contracts/src/tournament/factories/multilevel/MiddleTournamentFactory.sol
+++ prt/contracts/src/tournament/factories/multilevel/MiddleTournamentFactory.sol
@@ -3,46 +3,61 @@
 
 pragma solidity ^0.8.17;
 
+import {Clones} from "@openzeppelin-contracts-5.2.0/proxy/Clones.sol";
+
 import "prt-contracts/tournament/abstracts/NonLeafTournament.sol";
 import "prt-contracts/tournament/concretes/MiddleTournament.sol";
-
-import "prt-contracts/types/TournamentParameters.sol";
 import "prt-contracts/tournament/factories/IMultiLevelTournamentFactory.sol";
-
-import "prt-contracts/types/Machine.sol";
-import "prt-contracts/types/Tree.sol";
 import "prt-contracts/tournament/libs/Time.sol";
+import "prt-contracts/types/Machine.sol";
+import "prt-contracts/types/TournamentParameters.sol";
+import "prt-contracts/types/Tree.sol";
 
 contract MiddleTournamentFactory {
-    constructor() {}
+    using Clones for address;
+
+    MiddleTournament immutable _impl;
+
+    constructor(MiddleTournament impl) {
+        _impl = impl;
+    }
 
     function instantiate(
-        Machine.Hash _initialHash,
-        Tree.Node _contestedCommitmentOne,
-        Machine.Hash _contestedFinalStateOne,
-        Tree.Node _contestedCommitmentTwo,
-        Machine.Hash _contestedFinalStateTwo,
-        Time.Duration _allowance,
-        uint256 _startCycle,
-        uint64 _level,
-        TournamentParameters memory _tournamentParameters,
-        IDataProvider _provider,
-        IMultiLevelTournamentFactory _tournamentFactory
+        Machine.Hash initialHash,
+        Tree.Node contestedCommitmentOne,
+        Machine.Hash contestedFinalStateOne,
+        Tree.Node contestedCommitmentTwo,
+        Machine.Hash contestedFinalStateTwo,
+        Time.Duration allowance,
+        uint256 startCycle,
+        uint64 level,
+        TournamentParameters memory tournamentParameters,
+        IDataProvider provider,
+        IMultiLevelTournamentFactory tournamentFactory
     ) external returns (MiddleTournament) {
-        MiddleTournament _tournament = new MiddleTournament(
-            _initialHash,
-            _contestedCommitmentOne,
-            _contestedFinalStateOne,
-            _contestedCommitmentTwo,
-            _contestedFinalStateTwo,
-            _allowance,
-            _startCycle,
-            _level,
-            _tournamentParameters,
-            _provider,
-            _tournamentFactory
-        );
-
-        return _tournament;
+        MiddleTournament.Args memory args = MiddleTournament.Args({
+            tournamentArgs: TournamentArgs({
+                initialHash: initialHash,
+                startCycle: startCycle,
+                level: level,
+                levels: tournamentParameters.levels,
+                log2step: tournamentParameters.log2step,
+                height: tournamentParameters.height,
+                startInstant: Time.currentTime(),
+                allowance: allowance,
+                maxAllowance: tournamentParameters.maxAllowance,
+                matchEffort: tournamentParameters.matchEffort,
+                provider: provider
+            }),
+            nonRootTournamentArgs: NonRootTournamentArgs({
+                contestedCommitmentOne: contestedCommitmentOne,
+                contestedFinalStateOne: contestedFinalStateOne,
+                contestedCommitmentTwo: contestedCommitmentTwo,
+                contestedFinalStateTwo: contestedFinalStateTwo
+            }),
+            tournamentFactory: tournamentFactory
+        });
+        address clone = address(_impl).cloneWithImmutableArgs(abi.encode(args));
+        return MiddleTournament(clone);
     }
 }
