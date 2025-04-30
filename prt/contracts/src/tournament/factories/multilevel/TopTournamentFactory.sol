fc018e9161a95761fc8634eefb08529f6ecabd51
Guilherme Dantas <guidanoli@proton.me>
2025-04-30 11:11:34 -0300

    refactor!: adapt top tournament and factory

--- prt/contracts/src/tournament/factories/multilevel/TopTournamentFactory.sol
+++ prt/contracts/src/tournament/factories/multilevel/TopTournamentFactory.sol
@@ -3,23 +3,46 @@
 
 pragma solidity ^0.8.17;
 
-import "prt-contracts/tournament/concretes/TopTournament.sol";
+import {Clones} from "@openzeppelin-contracts-5.2.0/proxy/Clones.sol";
 
+import "prt-contracts/tournament/concretes/TopTournament.sol";
 import "prt-contracts/types/TournamentParameters.sol";
 
 contract TopTournamentFactory {
-    constructor() {}
+    using Clones for address;
+
+    uint64 constant START_CYCLE = 0;
+    uint64 constant LEVEL = 0;
+
+    TopTournament immutable _impl;
+
+    constructor(TopTournament impl) {
+        _impl = impl;
+    }
 
     function instantiate(
-        Machine.Hash _initialHash,
-        TournamentParameters memory _tournamentParameters,
-        IDataProvider _provider,
-        IMultiLevelTournamentFactory _tournamentFactory
+        Machine.Hash initialHash,
+        TournamentParameters memory tournamentParameters,
+        IDataProvider provider,
+        IMultiLevelTournamentFactory tournamentFactory
     ) external returns (TopTournament) {
-        TopTournament _tournament = new TopTournament(
-            _initialHash, _tournamentParameters, _provider, _tournamentFactory
-        );
-
-        return _tournament;
+        TopTournament.Args memory args = TopTournament.Args({
+            tournamentArgs: TournamentArgs({
+                initialHash: initialHash,
+                startCycle: START_CYCLE,
+                level: LEVEL,
+                levels: tournamentParameters.levels,
+                log2step: tournamentParameters.log2step,
+                height: tournamentParameters.height,
+                startInstant: Time.currentTime(),
+                allowance: tournamentParameters.maxAllowance,
+                maxAllowance: tournamentParameters.maxAllowance,
+                matchEffort: tournamentParameters.matchEffort,
+                provider: provider
+            }),
+            tournamentFactory: tournamentFactory
+        });
+        address clone = address(_impl).cloneWithImmutableArgs(abi.encode(args));
+        return TopTournament(clone);
     }
 }
