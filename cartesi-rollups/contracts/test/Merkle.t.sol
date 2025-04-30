41df8111ee01e5c208b55dcb40e6d3f4160b760d
Guilherme Dantas <guidanoli@proton.me>
2025-04-30 14:20:10 -0300

    fix: imports of `forge-std`

--- cartesi-rollups/contracts/test/Merkle.t.sol
+++ cartesi-rollups/contracts/test/Merkle.t.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import {Test} from "forge-std/Test.sol";
+import {Test} from "forge-std-1.9.6/src/Test.sol";
 
 import {MerkleConstants} from "src/MerkleConstants.sol";
 import {PristineMerkleTree} from "src/PristineMerkleTree.sol";
