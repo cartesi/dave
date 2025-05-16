02ea0a6e26727851ba54694df10cf8d0a837298c
Guilherme Dantas <guidanoli@proton.me>
2025-05-16 09:30:39 -0300

    refactor: deploy Cartesi Rollups devnet with Cannon

---
+++ cartesi-rollups/contracts/src/CannonDependencies.sol
@@ -0,0 +1,8 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+pragma solidity ^0.8.0;
+
+// List of contracts used in Cannonfiles
+
+import "cartesi-rollups-contracts-2.0.0/dapp/ApplicationFactory.sol";
