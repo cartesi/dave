045cc6d610ea5e4651b28436041eceba2d1b9da8
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2025-05-22 09:17:20 -0300

    refactor(prt-tests): cleanup test scripts (minor)

--- prt/tests/rollups/prt_rollups.lua
+++ prt/tests/rollups/prt_rollups.lua
@@ -1,13 +1,19 @@
 require "setup_path"
 
--- consensus contract address in anvil deployment
-local CONSENSUS_ADDRESS = assert(os.getenv("CONSENSUS"))
--- input contract address in anvil deployment
-local INPUT_BOX_ADDRESS = assert(os.getenv("INPUT_BOX"))
--- app contract address in anvil deployment
+-- anvil deployment state dump
+local ANVIL_PATH = assert(os.getenv("ANVIL_PATH"))
+
+-- machine template hash
+local TEMPLATE_MACHINE = assert(os.getenv("TEMPLATE_MACHINE"))
+
+-- addresses
 local APP_ADDRESS = assert(os.getenv("APP"))
+local CONSENSUS_ADDRESS = assert(os.getenv("CONSENSUS"))
+local INPUT_BOX_ADDRESS = assert(os.getenv("INPUT_BOX"))
+
 -- number of epochs to run the rollups test
-local MAX_EPOCH = tonumber(os.getenv("MAX_EPOCH")) or false
+local MAX_EPOCH = tonumber(os.getenv("MAX_EPOCH")) or math.maxinteger
+
 
 -- amount of time sleep between each react
 local SLEEP_TIME = 2
@@ -36,103 +42,40 @@ local ECHO_MSG = "0x48656c6c6f2076726f6d204461766521"
 
 -- Required Modules
 local new_scoped_require = require "test_utils.scoped_require"
-
 local helper = require "utils.helper"
-local blockchain_utils = require "blockchain.utils"
 local time = require "utils.time"
-local blockchain_constants = require "blockchain.constants"
 local Blockchain = require "blockchain.node"
 local Dave = require "dave.node"
-local Hash = require "cryptography.hash"
-local Machine = require "computation.machine"
-local MerkleBuilder = require "cryptography.merkle_builder"
 local Reader = require "dave.reader"
 local Sender = require "dave.sender"
-local rpath = assert(io.popen("realpath " .. assert(os.getenv("MACHINE_PATH"))))
-local rollups_machine_path = assert(rpath:read())
-rpath:close()
 
-os.execute("rm -rf _state")
+-- Main Execution
+local _blockchain_node = Blockchain:new(ANVIL_PATH)
+local reader = Reader:new()
+local sender = Sender:new(INPUT_BOX_ADDRESS, APP_ADDRESS)
+time.sleep(NODE_DELAY)
 
-local ROOT_LEAFS_QUERY =
-[[sqlite3 ./_state/%d/db \
-'SELECT level,base_cycle,compute_leaf_index,repetitions,HEX(compute_leaf) FROM compute_leafs WHERE level=0 ORDER BY compute_leaf_index ASC']]
-local function build_root_commitment_from_db(machine_path, epoch_index)
-    local builder = MerkleBuilder:new()
-    local machine = Machine:new_from_path(machine_path)
-    local initial_state = machine:state()
-    local handle = io.popen(string.format(ROOT_LEAFS_QUERY, epoch_index))
-    assert(handle)
-    local rows = handle:read "*a"
-    handle:close()
+local dave_node = Dave:new(TEMPLATE_MACHINE, APP_ADDRESS, SLEEP_TIME)
+time.sleep(NODE_DELAY)
 
-    if rows:find "Error" then
-        error(string.format("Read leafs failed:\n%s", rows))
-    end
 
-    -- Iterate over each line in the input data
-    for line in rows:gmatch("[^\n]+") do
-        local _, _, _, repetitions, compute_leaf = line:match(
-            "([^|]+)|([^|]+)|([^|]+)|([^|]+)|([^|]+)")
-        -- Convert values to appropriate types
-        repetitions = tonumber(repetitions)
-        compute_leaf = Hash:from_digest_hex("0x" .. compute_leaf)
-
-        builder:add(compute_leaf, repetitions)
-    end
-
-    return builder:build(initial_state.root_hash)
-end
-
-local INPUTS_QUERY =
-[[sqlite3 ./_state/%d/db 'select HEX(input)
-from inputs ORDER BY input_index ASC']]
-local function get_inputs_from_db(epoch_index)
-    local handle = io.popen(string.format(INPUTS_QUERY, epoch_index))
-    assert(handle)
-    local rows = handle:read "*a"
-    handle:close()
-
-    if rows:find "Error" then
-        error(string.format("Read inputs failed:\n%s", rows))
-    end
-
-    local inputs = {}
-    -- Iterate over each line in the input data
-    for line in rows:gmatch("[^\n]+") do
-        local input = line:match("([^|]+)")
-        table.insert(inputs, "0x" .. input)
-    end
-
-    return inputs
-end
-
-local MACHINE_PATH_QUERY = [[
-sqlite3 ./_state/db.sqlite3 \
-'SELECT s.file_path FROM epoch_snapshot_info AS e JOIN machine_state_snapshots AS s ON s.state_hash = e.state_hash WHERE e.epoch_number = %d']]
-local function get_machine_path(epoch_index)
-    local cmd = string.format(MACHINE_PATH_QUERY, epoch_index)
-    local handle = io.popen(cmd)
-    assert(handle)
-    local path = handle:read()
-    local tail = handle:read "*a"
-    handle:close()
-    if path:find "Error" or tail:find "Error" then
-        error(string.format("Read machine path failed:\n%s", path))
-    end
-    return path
-end
-
+print("Hello from Dave rollups lua prototype!")
+print(string.format("rollups test will run for %d epoch(s)", MAX_EPOCH))
 
 -- Function to setup players
 local function setup_players(root_tournament, epoch_index)
     local player_coroutines = {}
     local player_index = 1
-    local machine_path = get_machine_path(epoch_index)
+
+    local machine_path = dave_node:machine_path(epoch_index)
     print("machine path: {" .. machine_path ..  "}")
+
+    -- TODO: get from blockchain
+    local inputs = dave_node:inputs(epoch_index)
+
+    -- TODO: compute locally rather than getting from the node
     print("Calculating root commitment...")
-    local root_commitment = build_root_commitment_from_db(machine_path, epoch_index)
-    local inputs = get_inputs_from_db(epoch_index)
+    local root_commitment = dave_node:root_commitment(epoch_index)
 
     if FAKE_COMMITMENT_COUNT > 0 then
         print(string.format("Setting up dishonest player with %d fake commitments", FAKE_COMMITMENT_COUNT))
@@ -189,7 +132,7 @@ local function run_players(player_coroutines)
         -- 4 consecutive idle will advance blockchain for faster outcome
         if idle and idle_1 and idle_2 and idle_3 then
             print(string.format("All players idle, fastforward blockchain for %d blocks...", FAST_FORWARD_TIME))
-            blockchain_utils.advance_time(FAST_FORWARD_TIME, blockchain_constants.endpoint)
+            sender:advance_blocks(FAST_FORWARD_TIME)
         end
         idle_3 = idle_2
         idle_2 = idle_1
@@ -199,26 +142,6 @@ local function run_players(player_coroutines)
     end
 end
 
--- Main Execution
-local _blockchain_node = Blockchain:new(rollups_machine_path .. "/anvil_state.json")
-time.sleep(NODE_DELAY)
-
--- trace, debug, info, warn, error
-local verbosity = os.getenv("VERBOSITY") or 'debug'
--- 0, 1, full
-local trace_level = os.getenv("TRACE_LEVEL") or 'full'
-local _dave_node = Dave:new(rollups_machine_path .. "/machine-image", APP_ADDRESS, SLEEP_TIME, verbosity, trace_level)
-time.sleep(NODE_DELAY)
-
-local reader = Reader:new(blockchain_constants.endpoint)
-local sender = Sender:new(blockchain_constants.pks[1], blockchain_constants.endpoint)
-
-print("Hello from Dave rollups lua prototype!")
-if MAX_EPOCH then
-    print(string.format("rollups test will only run %d epoch(s)", MAX_EPOCH))
-end
-
-local input_index = 1
 while true do
     local sealed_epochs = reader:read_epochs_sealed(CONSENSUS_ADDRESS)
 
@@ -231,9 +154,7 @@ while true do
         local epoch = #sealed_epochs - 1
         print("last sealed epoch: " .. epoch)
         local last_sealed_epoch = sealed_epochs[#sealed_epochs]
-        for _ = input_index, input_index + 2 do
-            sender:tx_add_input(INPUT_BOX_ADDRESS, APP_ADDRESS, ECHO_MSG)
-        end
+        sender:tx_add_inputs(INPUT_BOX_ADDRESS, APP_ADDRESS, {ECHO_MSG, ECHO_MSG, ECHO_MSG})
 
         -- react to last sealed epoch
         local root_tournament = sealed_epochs[#sealed_epochs].tournament
