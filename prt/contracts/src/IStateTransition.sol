b32f2de790211e798dd0bb2c6a8a804dc10fbd8f
Guilherme Dantas <guidanoli@proton.me>
2025-05-26 08:48:11 -0300

    docs: add template for transitionState

--- prt/contracts/src/IStateTransition.sol
+++ prt/contracts/src/IStateTransition.sol
@@ -20,6 +20,12 @@ import "prt-contracts/IDataProvider.sol";
 /// @title IStateTransition
 /// @notice Transitions machine state from s to s+1
 interface IStateTransition {
+    /// @notice ?
+    /// @param machineState ?
+    /// @param counter ?
+    /// @param proofs ?
+    /// @param provider ?
+    /// @return ?
     function transitionState(
         bytes32 machineState,
         uint256 counter,
