3531a0a3e25b740dddbe8fdc32e2196d478c4599
Bruno Menezes <brunodmenezes@gmail.com>
2025-06-26 11:49:02 +1200

    chore: remove daisyui / tailwindcss

--- visualization/ui/vite.config.ts
+++ visualization/ui/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
 });
