7552f666ce34c1c2f4ee2cd83968c9cd643a0bc6
Bruno Menezes <brunodmenezes@gmail.com>
2025-06-26 12:13:35 +1200

    chore: Remove all graphQL and current components

--- visualization/ui/src/App.tsx
+++ visualization/ui/src/App.tsx
@@ -1,24 +1,11 @@
 import { Route, Routes } from "react-router";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { Layout } from "./components/Layout";
-import { Redirect } from "./components/Redirect";
 import Home from "./home/Home";
-import TournamentRoutes from "./tournament/routes";
 
 function App() {
   return (
-    <ErrorBoundary fallback={<p>Hummm...</p>}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-
-          {TournamentRoutes()}
-
-          {/* Catch-all route */}
-          <Route path="*" element={<Redirect to="/" />} />
-        </Route>
-      </Routes>
-    </ErrorBoundary>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
