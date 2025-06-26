3b9090d5fe6ec78b86d1089fc63ec1bc913ce6ea
Bruno Menezes <brunodmenezes@gmail.com>
2025-06-26 12:34:13 +1200

    chore: Add mantine-ui.

--- visualization/ui/src/home/Home.tsx
+++ visualization/ui/src/home/Home.tsx
@@ -1,7 +1,12 @@
+import { Stack } from "@mantine/core";
 import type { FC } from "react";
 
 const Home: FC = () => {
-  return <h1>Home Placeholder</h1>;
+  return (
+    <Stack justify="center" align="center" h="100%">
+      <h1>Home Placeholder</h1>;
+    </Stack>
+  );
 };
 
 export default Home;
