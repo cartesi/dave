abe09c044ad83cabd8a7e3af8c0f540a36f1f33d
Bruno Menezes <brunodmenezes@gmail.com>
2025-08-05 17:44:44 +1200

    feat(visualization): Add application components and simple data mocks.

---
+++ visualization/ui/src/providers/StyleProvider.tsx
@@ -0,0 +1,7 @@
+import { MantineProvider } from "@mantine/core";
+import type { FC, PropsWithChildren } from "react";
+import theme from "./theme";
+
+export const StyleProvider: FC<PropsWithChildren> = ({ children }) => {
+  return <MantineProvider theme={theme}>{children}</MantineProvider>;
+};
