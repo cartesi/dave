cf00c3669affa1bae13a231baca6d3c64aeab395
Zehui Zheng <work996mail@gmail.com>
2025-08-11 02:23:52 -0700

    feat: bonds

--- cartesi-rollups/contracts/src/DaveConsensus.sol
+++ cartesi-rollups/contracts/src/DaveConsensus.sol
@@ -156,6 +156,7 @@ contract DaveConsensus is IDataProvider, IOutputsMerkleRootValidator, ERC165 {
         // Check tournament finished
         (bool isFinished,, Machine.Hash finalMachineStateHash) = _tournament.arbitrationResult();
         require(isFinished, TournamentNotFinishedYet());
+        _tournament.recoverBond();
         _tournament = ITournament(address(0));
 
         // Check outputs Merkle root
