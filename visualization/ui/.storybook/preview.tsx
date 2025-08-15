f3241c1b6e36cf7446b2a48b04a7f4b935ed43b9
Bruno Menezes <brunodmenezes@gmail.com>
2025-08-15 17:17:57 +1200

    feat(visualization): Apply formatter rules on storybook folder

--- visualization/ui/.storybook/preview.tsx
+++ visualization/ui/.storybook/preview.tsx
@@ -2,59 +2,58 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import type { Preview, StoryContext, StoryFn } from "@storybook/react-vite";
 import { BrowserRouter } from "react-router";
-import Layout from '../src/components/layout/Layout';
+import Layout from "../src/components/layout/Layout";
 import theme from "../src/providers/theme";
 
 const withRouter = (StoryFn: StoryFn, context: StoryContext) => (
-  <BrowserRouter>{StoryFn(context.args, context)}</BrowserRouter>
+    <BrowserRouter>{StoryFn(context.args, context)}</BrowserRouter>
 );
 
-const withLayout = (StoryFn: StoryFn, context:StoryContext) => {
-  const {title} = context;
-  const [sectionType] = title.split('/');
-  
-  if(sectionType.toLowerCase().includes('pages')) 
-    return <Layout>{StoryFn(context.args, context)}</Layout>
+const withLayout = (StoryFn: StoryFn, context: StoryContext) => {
+    const { title } = context;
+    const [sectionType] = title.split("/");
 
+    if (sectionType.toLowerCase().includes("pages"))
+        return <Layout>{StoryFn(context.args, context)}</Layout>;
 
-  return <>{StoryFn(context.args, context)}</>
-
-}
+    return <>{StoryFn(context.args, context)}</>;
+};
 const withMantine = (StoryFn: StoryFn, context: StoryContext) => {
-  const currentBg = context.globals.backgrounds?.value ?? "light";      
-  
-  return (
-    <>
-     <MantineProvider forceColorScheme={currentBg} theme={theme}>         
-        {StoryFn(context.args, context)}
-      </MantineProvider>
-    </>
-  )
+    const currentBg = context.globals.backgrounds?.value ?? "light";
+
+    return (
+        <>
+            <MantineProvider forceColorScheme={currentBg} theme={theme}>
+                {StoryFn(context.args, context)}
+            </MantineProvider>
+        </>
+    );
 };
 
 const preview: Preview = {
-  initialGlobals: {
-    backgrounds: { value: "light" },
-  },
-  parameters: {
-    backgrounds: {
-      options: {
-        light: { name: "light", value: "#ffffffff" },
-        dark: { name: "dark", value: "#333" },
-      },
+    initialGlobals: {
+        backgrounds: { value: "light" },
     },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        backgrounds: {
+            options: {
+                light: { name: "light", value: "#ffffffff" },
+                dark: { name: "dark", value: "#333" },
+            },
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
-  decorators: [
-    // Order matters. So layout decorator first. Fn calling is router(mantine(layout))
-    withLayout, 
-    withMantine, 
-    withRouter ],
+    decorators: [
+        // Order matters. So layout decorator first. Fn calling is router(mantine(layout))
+        withLayout,
+        withMantine,
+        withRouter,
+    ],
 };
 
 export default preview;
