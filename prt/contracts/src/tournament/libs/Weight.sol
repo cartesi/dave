c20120c278ba76cbd169f2b664bf627b7d58d6b6
Zehui Zheng <work996mail@gmail.com>
2025-08-21 22:24:32 -0700

    feat: adjust weight

--- prt/contracts/src/tournament/libs/Weight.sol
+++ prt/contracts/src/tournament/libs/Weight.sol
@@ -4,12 +4,15 @@
 pragma solidity ^0.8.17;
 
 library Weight {
-    uint256 constant ADVANCE_MATCH = 1;
-    uint256 constant WIN_MATCH_BY_TIMEOUT = 1;
-    uint256 constant ELIMINATE_MATCH_BY_TIMEOUT = 1;
-    uint256 constant SEAL_INNER_MATCH_AND_CREATE_INNER_TOURNAMENT = 3;
-    uint256 constant WIN_INNER_TOURNAMENT = 1;
-    uint256 constant ELIMINATE_INNER_TOURNAMENT = 1;
-    uint256 constant SEAL_LEAF_MATCH = 1;
-    uint256 constant WIN_LEAF_MATCH = 2;
+    uint256 constant TX_INTRINSIC_GAS = 21000;
+
+    uint256 constant ADVANCE_MATCH = 65175 + TX_INTRINSIC_GAS;
+    uint256 constant WIN_MATCH_BY_TIMEOUT = 86203 + TX_INTRINSIC_GAS;
+    uint256 constant ELIMINATE_MATCH_BY_TIMEOUT = 62135 + TX_INTRINSIC_GAS;
+    uint256 constant SEAL_INNER_MATCH_AND_CREATE_INNER_TOURNAMENT =
+        237531 + TX_INTRINSIC_GAS;
+    uint256 constant WIN_INNER_TOURNAMENT = 228030 + TX_INTRINSIC_GAS;
+    uint256 constant ELIMINATE_INNER_TOURNAMENT = 85183 + TX_INTRINSIC_GAS;
+    uint256 constant SEAL_LEAF_MATCH = 57355 + TX_INTRINSIC_GAS;
+    uint256 constant WIN_LEAF_MATCH = 102728 + TX_INTRINSIC_GAS;
 }
