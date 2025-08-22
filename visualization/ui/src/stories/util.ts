3462ffb01a985a53e00f636ad603d0074dbef7d5
Bruno Menezes <brunodmenezes@gmail.com>
2025-08-22 12:48:33 +1200

    refactor: use let instead of var.
    * No global usage detected.

--- visualization/ui/src/stories/util.ts
+++ visualization/ui/src/stories/util.ts
@@ -10,7 +10,7 @@ function mulberry32(seed: number) {
     return function () {
         seed |= 0;
         seed = (seed + 0x6d2b79f5) | 0;
-        var t = Math.imul(seed ^ (seed >>> 15), 1 | seed);
+        let t = Math.imul(seed ^ (seed >>> 15), 1 | seed);
         t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
         return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
     };
