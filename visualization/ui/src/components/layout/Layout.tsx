1faa5102eae3f0a9edefe11cabd91312cd7096b5
Bruno Menezes <brunodmenezes@gmail.com>
2025-09-08 12:33:19 +1200

    refactor: Change layout container strategy for height calculation.

--- visualization/ui/src/components/layout/Layout.tsx
+++ visualization/ui/src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ import {
     Group,
     useMantineTheme,
 } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
+import { useMediaQuery, useViewportSize } from "@mantine/hooks";
 import type { FC, PropsWithChildren } from "react";
 import { Link } from "react-router";
 import CartesiLogo from "../icons/CartesiLogo";
@@ -14,18 +14,30 @@ import CartesiLogo from "../icons/CartesiLogo";
 const Layout: FC<PropsWithChildren> = ({ children }) => {
     const theme = useMantineTheme();
     const isSmallDevice = useMediaQuery(`(max-width:${theme.breakpoints.sm})`);
+    const { height } = useViewportSize();
 
     return (
         <AppShell>
             <AppShellHeader>
-                <Group h="100%" justify="space-between" align="center" px="lg">
+                <Group
+                    h="100%"
+                    justify="space-between"
+                    align="center"
+                    px={isSmallDevice ? "xs" : "lg"}
+                >
                     <Link to="/" aria-label="Home">
                         <CartesiLogo height={isSmallDevice ? 30 : 40} />
                     </Link>
                 </Group>
             </AppShellHeader>
             <AppShellMain>
-                <Container px={isSmallDevice ? "0" : ""}>{children}</Container>
+                <Container
+                    px={isSmallDevice ? "sm" : ""}
+                    mih={`calc(${height}px - var(--app-shell-header-height))`}
+                    strategy="grid"
+                >
+                    {children}
+                </Container>
             </AppShellMain>
         </AppShell>
     );
