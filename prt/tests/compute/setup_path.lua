f2a757ea9596ab6dfae50951b1cc5e617eea927e
Stephen Chen <20940639+stephenctw@users.noreply.github.com>
2025-09-09 16:39:25 +0800

    chore(client-tests): remove `compute`

--- prt/tests/compute/setup_path.lua
+++
@@ -1,7 +0,0 @@
--- setup client-lua path
-package.path = package.path .. ";../common/?.lua"
-package.path = package.path .. ";../../client-lua/?.lua"
-
--- setup cartesi machine path
-package.path = package.path .. ";/opt/cartesi/lib/lua/5.4/?.lua"
-package.cpath = package.cpath .. ";/opt/cartesi/lib/lua/5.4/?.so"
