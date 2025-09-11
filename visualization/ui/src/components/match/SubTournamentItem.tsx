dea03d51456f797a5015dae3e665386801e42daa
Bruno Menezes <brunodmenezes@gmail.com>
2025-09-11 21:00:48 +1200

    refactor(visualization): Adjust sub-tournament-item to link to the correct tournament path.

--- visualization/ui/src/components/match/SubTournamentItem.tsx
+++ visualization/ui/src/components/match/SubTournamentItem.tsx
@@ -8,6 +8,11 @@ import {
 } from "@mantine/core";
 import { type FC } from "react";
 import { TbTrendingDown } from "react-icons/tb";
+import { Link, useParams } from "react-router";
+import {
+    routePathBuilder,
+    type RoutePathParams,
+} from "../../routes/routePathBuilder";
 import { CycleRangeFormatted } from "../CycleRangeFormatted";
 import type { Claim, CycleRange } from "../types";
 import { ClaimTimelineItem } from "./ClaimTimelineItem";
@@ -41,7 +46,11 @@ export interface SubTournamentItemProps {
 
 export const SubTournamentItem: FC<SubTournamentItemProps> = (props) => {
     const { claim, level, now, range, timestamp } = props;
-
+    const params = useParams<RoutePathParams>();
+    const subTournamentUrl =
+        level === "middle"
+            ? routePathBuilder.middleTournament(params)
+            : routePathBuilder.bottomTournament(params);
     const theme = useMantineTheme();
     const scheme = useComputedColorScheme();
     const bg = scheme === "light" ? theme.colors.gray[0] : undefined;
@@ -53,7 +62,13 @@ export const SubTournamentItem: FC<SubTournamentItemProps> = (props) => {
                     <Stack gap="xs">
                         <CycleRangeFormatted size="xs" range={range} />
                     </Stack>
-                    <Button rightSection={<TbTrendingDown />}>{level}</Button>
+                    <Button
+                        component={Link}
+                        to={subTournamentUrl}
+                        rightSection={<TbTrendingDown />}
+                    >
+                        {level}
+                    </Button>
                 </Group>
             </Paper>
         </ClaimTimelineItem>
