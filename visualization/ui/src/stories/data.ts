926c8e3d358c6b29f4621fa975df517be6e24196
Bruno Menezes <brunodmenezes@gmail.com>
2025-10-02 17:59:41 +1300

    feat(visualization): Simulate match actions and sub-tournaments.

--- visualization/ui/src/stories/data.ts
+++ visualization/ui/src/stories/data.ts
@@ -1,13 +1,21 @@
-import { getUnixTime, subMinutes } from "date-fns";
-import { keccak256 } from "viem";
+import {
+    addMinutes,
+    addSeconds,
+    fromUnixTime,
+    getUnixTime,
+    subMinutes,
+} from "date-fns";
+import { keccak256, type Hex } from "viem";
 import type {
     Application,
+    Claim,
+    CycleRange,
     Epoch,
     Match,
     MatchAction,
     Tournament,
 } from "../components/types";
-import { generateMatchID, generateTournamentId } from "./util";
+import { claim, generateMatchID, generateTournamentId } from "./util";
 
 export type EpochWithTournament = Epoch & { tournament?: Tournament };
 export type ApplicationEpochs = Application & { epochs: EpochWithTournament[] };
@@ -22,7 +30,10 @@ export const dummyMatch: Match = {
 
 const currentDate = new Date();
 
-export const applications: ApplicationEpochs[] = [
+// large 40kb proof
+const proof = `0x${"00".repeat(1024 * 40)}` as Hex;
+
+const applications: ApplicationEpochs[] = [
     {
         address: "0x4c1E74EF88a75C24e49eddD9f70D82A94D19251c",
         name: "honeypot",
@@ -211,3 +222,214 @@ export const applications: ApplicationEpochs[] = [
         epochs: [],
     },
 ];
+
+const getTournamentHeight = (level: Tournament["level"]) => {
+    switch (level) {
+        case "top":
+            return 48;
+        case "middle":
+            return 27;
+        default:
+            return 17;
+    }
+};
+
+const getNextRange = (level: Tournament["level"]): CycleRange => {
+    switch (level) {
+        case "top":
+            return [7_102_817_919, 7_402_918_071];
+        case "middle":
+            return [7_204_918_919, 7_205_024_571];
+        default:
+            throw new Error(`${level} does not create a sub-tournament`);
+    }
+};
+
+const numberOfClaims = {
+    top: 32,
+    middle: 24,
+    bottom: 16,
+};
+
+let tournamentCounter = 1;
+let claimCounter = 0;
+
+const getMinutesPerLevel = (level: Tournament["level"]) => {
+    switch (level) {
+        case "middle":
+            return 60;
+        case "bottom":
+            return 30;
+        default:
+            return 120;
+    }
+};
+
+interface Config {
+    parentMatches?: Match[];
+    now: Date;
+    level: Tournament["level"];
+}
+
+type TournamentGeneratorParams = {
+    startCycle: number;
+    endCycle: number;
+    level: Tournament["level"];
+    parentMatches?: Match[];
+    match?: Match;
+    now: Date;
+};
+
+interface SimulateActionsParams extends Config {
+    match: Match;
+}
+
+const simulateActions = (config: SimulateActionsParams) => {
+    const height = getTournamentHeight(config.level);
+    const elapseTime = getMinutesPerLevel(config.level);
+    const matchTime = fromUnixTime(config.match.timestamp);
+    const actions: MatchAction[] = Array.from({ length: height }, (_, i) => ({
+        type: "advance",
+        direction: i % 2 === 0 ? 0 : 1,
+        timestamp: getUnixTime(subMinutes(matchTime, elapseTime - i)),
+    }));
+
+    if (config.level === "bottom") {
+        // XXX: Needs to bubble up.
+        const winner = actions.length % 2 === 0 ? 1 : 2;
+        const lastAction: MatchAction = {
+            type: "leaf_match_sealed",
+            proof,
+            timestamp: getUnixTime(
+                subMinutes(matchTime, elapseTime - actions.length),
+            ),
+            winner,
+        };
+
+        actions.push(lastAction);
+        config.match.actions = actions;
+        config.match.winner = winner;
+        config.parentMatches?.forEach((match) => {
+            if (!match.winner) {
+                match.winner = winner;
+                match.actions.push(lastAction);
+            }
+        });
+    } else {
+        const nextLevel = config.level === "top" ? "middle" : "bottom";
+        const range = getNextRange(config.level);
+        actions.push({
+            type: "match_sealed_inner_tournament_created",
+            range,
+            timestamp: getUnixTime(
+                subMinutes(matchTime, elapseTime - actions.length),
+            ),
+        });
+        config.match.actions = actions;
+        const parentMatches = config.parentMatches ? config.parentMatches : [];
+        parentMatches.push(config.match);
+        config.match.tournament = generateTournament({
+            now: fromUnixTime(config.match.timestamp),
+            level: nextLevel,
+            startCycle: range[0],
+            endCycle: range[1],
+            match: config.match,
+            parentMatches,
+        });
+    }
+};
+
+const generateMatches = ({ parentMatches = [], now, level }: Config) => {
+    const totalClaims = numberOfClaims[level];
+    const matches: Match[] = [];
+    const claims: Claim[] = Array.from({ length: totalClaims }).map(() =>
+        claim(claimCounter++),
+    );
+    let danglingClaim = undefined;
+    let newClaim = claims.shift();
+    let nextDatetime = now;
+    let matchCounter = 1;
+
+    while (newClaim) {
+        if (level === "top") {
+            // console.clear();
+            console.log(`claims: ${claims.length}`);
+        }
+        if (danglingClaim) {
+            // create a match with the dangling claim
+            const claim1 = danglingClaim;
+            const match: Match = {
+                id: generateMatchID(claim1.hash, newClaim.hash),
+                actions: [],
+                claim1,
+                claim2: newClaim,
+                timestamp: getUnixTime(nextDatetime),
+            };
+
+            simulateActions({
+                level,
+                match,
+                now: nextDatetime,
+                parentMatches,
+            });
+
+            matches.push(match);
+            danglingClaim = undefined;
+
+            nextDatetime = addMinutes(nextDatetime, 2);
+
+            matchCounter++;
+
+            if (match.winner) {
+                match.winnerTimestamp = getUnixTime(nextDatetime);
+                nextDatetime = addSeconds(nextDatetime, 27);
+                const winnerClaim =
+                    match.winner === 1 ? match.claim1 : match.claim2;
+                claims.unshift(winnerClaim);
+            }
+        } else {
+            // assign the claim to the dangling slot
+            danglingClaim = newClaim;
+        }
+
+        newClaim = claims.shift();
+    }
+
+    return {
+        danglingClaim,
+        matches,
+    };
+};
+
+const generateTournament = (params: TournamentGeneratorParams): Tournament => {
+    const tournament: Tournament = {
+        id: generateTournamentId(tournamentCounter++, 0),
+        startCycle: params.startCycle,
+        endCycle: params.endCycle,
+        danglingClaim: undefined,
+        height: getTournamentHeight(params.level),
+        level: params.level,
+        matches: [],
+    };
+
+    const { danglingClaim, matches } = generateMatches({
+        level: params.level,
+        now: currentDate,
+        parentMatches: params.parentMatches,
+    });
+
+    tournament.matches = matches;
+    tournament.danglingClaim = danglingClaim;
+
+    return tournament;
+};
+
+// XXX: Generate only for the disputed epoch.
+applications[0].epochs[3].tournament = generateTournament({
+    now: currentDate,
+    startCycle: 5_911_918_810,
+    endCycle: 9_918_817_817,
+    level: "top",
+});
+
+export { applications };
