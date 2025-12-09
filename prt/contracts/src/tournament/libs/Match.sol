8329f783b27e0f78ba157c159fc6e9d9472f9795
Henrique Santos <henriquemarloncs@gmail.com>
2025-12-08 21:55:50 -0300

    fix: improve leaf index validation

--- prt/contracts/src/tournament/libs/Match.sol
+++ prt/contracts/src/tournament/libs/Match.sol
@@ -204,10 +204,7 @@ library Match {
         return args.toCycle(state.runningLeafPosition);
     }
 
-    function getDivergence(
-        State memory state,
-        Commitment.Arguments memory args
-    )
+    function getDivergence(State memory state, Commitment.Arguments memory args)
         internal
         pure
         returns (
